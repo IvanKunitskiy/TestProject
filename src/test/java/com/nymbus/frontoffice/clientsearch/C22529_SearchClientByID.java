ebfae4cc964cdaa298d2de70d3c24148ebbf39c9
dbarsukov <dbarsukov@nymbus.com>
2020-02-17 18:15:31 +0200

    added first steps tot est case

--- src/test/java/com/nymbus/frontoffice/clientsearch/C22529_SearchClientByID.java
+++ src/test/java/com/nymbus/frontoffice/clientsearch/C22529_SearchClientByID.java
@@ -1,7 +1,11 @@
 package com.nymbus.frontoffice.clientsearch;
 
+import com.nymbus.actions.Actions;
+import com.nymbus.actions.client.ClientsActions;
 import com.nymbus.base.BaseTest;
 import com.nymbus.models.client.Client;
+import com.nymbus.pages.Pages;
+import com.nymbus.util.Constants;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
@@ -16,11 +20,24 @@ public class C22529_SearchClientByID extends BaseTest {
 
     @BeforeMethod
     public void preCondition() {
-        client = new Client();
+        client = new Client().setDefaultClientData();
     }
 
     @Test(description = "C22529, Search client by ID")
     public void SearchByID() {
+        System.out.println(client.toString());
+        String clientID =  client.getClientID();
+        String clientFullName =  client.getLastName() + ", " + client.getFirstName();
+        System.out.println("----------> " + clientID);
+
+        navigateToUrl(Constants.URL);
+
+        LOG.info("Step 1: Log in to the system as the User from the precondition");
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
+        ClientsActions.createClient().createClient(client);
+
+        LOG.info("Step 2: Click within search field and try to search for an existing client (by client id)");
+
 
     }
 
