87e9716733e8be02cbb36915ff6e8d9937edeced
dbarsukov <dbarsukov@nymbus.com>
2020-02-17 17:18:11 +0200

    Started test case implementation

--- src/test/java/com/nymbus/frontoffice/clientsearch/C22529_SearchClientByID.java
+++ src/test/java/com/nymbus/frontoffice/clientsearch/C22529_SearchClientByID.java
@@ -1,4 +1,28 @@
 package com.nymbus.frontoffice.clientsearch;
 
-public class C22529_SearchClientByID {
+import com.nymbus.base.BaseTest;
+import com.nymbus.models.client.Client;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("User Managenment")
+@Owner("Dmytro")
+public class C22529_SearchClientByID extends BaseTest {
+    private Client client;
+
+    @BeforeMethod
+    public void preCondition() {
+        client = new Client();
+    }
+
+    @Test(description = "C22529, Search client by ID")
+    public void SearchByID() {
+
+    }
+
+
 }
