6a6a8b7f427e5eb6df2a5a2d8266fa9d61bcabe1
dbarsukov <dbarsukov@nymbus.com>
2020-02-19 13:33:38 +0200

    Added first 2 steps implementation to the test case

--- src/test/java/com/nymbus/frontoffice/clientsearch/C22531_SearchByLastFourDigitsOfTaxID.java
+++ src/test/java/com/nymbus/frontoffice/clientsearch/C22531_SearchByLastFourDigitsOfTaxID.java
@@ -1,4 +1,48 @@
 package com.nymbus.frontoffice.clientsearch;
 
-public class C22531_SearchByLastFourDigitsOfTaxID {
+import com.nymbus.actions.Actions;
+import com.nymbus.actions.client.ClientsActions;
+import com.nymbus.base.BaseTest;
+import com.nymbus.models.client.Client;
+import com.nymbus.pages.Pages;
+import com.nymbus.util.Constants;
+import io.qameta.allure.*;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("Clients search")
+@Severity(SeverityLevel.CRITICAL)
+@Owner("Dmytro")
+public class C22531_SearchByLastFourDigitsOfTaxID extends BaseTest {
+    private Client client;
+
+    @BeforeMethod
+    public void preConditions() {
+        client = new Client().setDefaultClientData();
+    }
+
+    @Test
+    public void searchByTaxID() {
+
+        LOG.info("Step 1: Log in to the system as the User from the precondition");
+        navigateToUrl(Constants.URL);
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
+
+        ClientsActions.createClient().createClient(client);
+        final String clientID = Pages.clientDetailsPage().getClientID();
+        final String clientTaxID = client.getTaxID();
+        Pages.aSideMenuPage().clickClientMenuItem();
+
+        LOG.info("Step 2: Click within search field and try to search for an existing client (by 4 last digits of tax id number)");
+        String taxIDQuery = clientTaxID.substring(5);
+        Pages.clientsPage().typeToClientsSearchInputField(taxIDQuery);
+
+        Assert.assertTrue(Pages.clientsPage().getAllLookupResults().size() > 0, "There are no relevant lookup results");
+        if (Pages.clientsPage().getAllLookupResults().size() == 8) {
+            Assert.assertTrue(Pages.clientsPage().isLoadMoreResultsButtonVisible(), "'Load more results' button is not visible in search lookup list");
+        }
+        Assert.assertTrue(Pages.clientsPage().isSearchResultsRelative(Pages.clientsPage().getAllLookupResults(), taxIDQuery), "Search results are not relevant");
+    }
 }
