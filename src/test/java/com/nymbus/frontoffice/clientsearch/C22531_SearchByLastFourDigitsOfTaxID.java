258beab18e467487d9434087fd1cd95ef3392db8
dbarsukov <dbarsukov@nymbus.com>
2020-02-20 14:16:49 +0200

    Added assertion in the 3rd step. Made minor fixes to the test case structure. Implemented getClientIDsFromSearchResults() method

--- src/test/java/com/nymbus/frontoffice/clientsearch/C22531_SearchByLastFourDigitsOfTaxID.java
+++ src/test/java/com/nymbus/frontoffice/clientsearch/C22531_SearchByLastFourDigitsOfTaxID.java
@@ -11,8 +11,6 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
 @Epic("Frontoffice")
 @Feature("Clients search")
 @Owner("Dmytro")
@@ -28,7 +26,6 @@ public class C22531_SearchByLastFourDigitsOfTaxID extends BaseTest {
     @Test(description = "C22531, Search by last four digits of TaxID")
     @Severity(SeverityLevel.CRITICAL)
     public void searchByTaxID() {
-
         LOG.info("Step 1: Log in to the system as the User from the precondition");
         navigateToUrl(Constants.URL);
         Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
@@ -39,7 +36,7 @@ public class C22531_SearchByLastFourDigitsOfTaxID extends BaseTest {
         Pages.aSideMenuPage().clickClientMenuItem();
 
         LOG.info("Step 2: Click within search field and try to search for an existing client (by 4 last digits of tax id number)");
-        String taxIDQuery = clientTaxID.substring(5);
+        final String taxIDQuery = clientTaxID.substring(5);
         Pages.clientsPage().typeToClientsSearchInputField(taxIDQuery);
 
         Assert.assertTrue(Pages.clientsPage().getAllLookupResults().size() > 0, "There are no relevant lookup results");
@@ -50,19 +47,6 @@ public class C22531_SearchByLastFourDigitsOfTaxID extends BaseTest {
 
         LOG.info("Step 3: Click the 'Search' button");
         Pages.clientsPage().clickOnSearchButton();
-
-        List<String> accountNumbers = Pages.clientsSearchResultsPage().getAccountNumbersFromSearchResults();
-        Assert.assertTrue(Pages.clientsPage().isSearchResultsRelative(accountNumbers, taxIDQuery));
-
-        // TODO: Need to implement assertion for tax id matches in the search list
-//        List<String> searchResults = Pages.clientsSearchResultsPage().getSearchResults();
-//        boolean isClientPresent = false;
-//
-//        for (String res : searchResults) {
-//            if (res.contains("-")) continue;
-//            if (res.split(("\n"))[1].equals(clientID)) isClientPresent = true;
-//        }
-//
-//        Assert.assertTrue(isClientPresent, "Client nor found in search results");
+        Assert.assertTrue(Pages.clientsSearchResultsPage().getClientIDsFromSearchResults().contains(clientID), "Client not found in search results by 'Client ID' criteria");
     }
 }
