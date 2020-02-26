b20cf47ffc3969b1e7763cc8ced1c6c953567699
dbarsukov <dbarsukov@nymbus.com>
2020-02-26 12:57:10 +0200

    added assertions in final step

--- src/test/java/com/nymbus/frontoffice/boxaccountsmanagement/C15042_ViewNewSafeBoxAccount.java
+++ src/test/java/com/nymbus/frontoffice/boxaccountsmanagement/C15042_ViewNewSafeBoxAccount.java
@@ -2,6 +2,7 @@ package com.nymbus.frontoffice.boxaccountsmanagement;
 
 import com.nymbus.actions.Actions;
 import com.nymbus.actions.account.AccountActions;
+import com.nymbus.actions.client.ClientsActions;
 import com.nymbus.base.BaseTest;
 import com.nymbus.models.account.Account;
 import com.nymbus.models.client.Client;
@@ -32,55 +33,35 @@ public class C15042_ViewNewSafeBoxAccount extends BaseTest {
     @Severity(SeverityLevel.CRITICAL)
     public void viewNewSafeBoxAccount() {
 
+        LOG.info("Step 1: Log in to Nymbus");
         navigateToUrl(Constants.URL);
         Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
 
-        final String clientID = "18506";
+        ClientsActions.createClient().createClient(client);
+        final String clientID = Pages.clientDetailsPage().getClientID();
+        AccountActions.createAccount().createAccount(safeDepositBoxAccount);
 
+        LOG.info("Step 2: Go to Clients screen and search for client from preconditions");
+        Pages.aSideMenuPage().clickClientMenuItem();
         Pages.clientsPage().typeToClientsSearchInputField(clientID);
+
         Assert.assertTrue(Pages.clientsPage().getAllLookupResults().size() == 1, "There is more than one client found");
         Assert.assertTrue(Pages.clientsPage().isSearchResultsRelative(Pages.clientsPage().getAllLookupResults(), clientID));
 
         Pages.clientsPage().clickOnSearchButton();
+
+        LOG.info("Step 3: Open the account");
         Pages.clientsSearchResultsPage().clickTheExactlyMatchedClientInSearchResults();
         Pages.clientDetailsPage().waitForPageLoaded();
         Pages.clientDetailsPage().clickAccountsTab();
+        Pages.clientDetailsPage().openAccountByNumber(safeDepositBoxAccount.getAccountNumber());
 
-        AccountActions.createAccount().createAccount(safeDepositBoxAccount);
-
-        // assert ProductType
-        // assert BoxSize
-        // assert AccountNumberValue
-        // assert AccountTitleValue
-        // assert BankBranch
-
-//        LOG.info("Step 1: Log in to Nymbus");
-//        navigateToUrl(Constants.URL);
-//        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
-//
-//        ClientsActions.createClient().createClient(client);
-//        AccountActions.createAccount().createAccount(safeDepositBoxAccount);
-//
-//        final String clientID = Pages.clientDetailsPage().getClientID();
-//
-//        LOG.info("Step 2: Go to Clients screen and search for client from preconditions");
-//        Pages.aSideMenuPage().clickClientMenuItem();
-//        Pages.clientsPage().typeToClientsSearchInputField(clientID);
-//        Assert.assertTrue(Pages.clientsPage().getAllLookupResults().size() == 1, "There is more than one client found");
-//        Assert.assertTrue(Pages.clientsPage().isSearchResultsRelative(Pages.clientsPage().getAllLookupResults(), clientID));
-//
-//        Pages.clientsPage().clickOnSearchButton();
-//
-//        LOG.info("Step 3: Open the account");
-//        Pages.clientsSearchResultsPage().clickTheExactlyMatchedClientInSearchResults();
-//        Pages.clientDetailsPage().waitForPageLoaded();
-//        Pages.clientDetailsPage().clickAccountsTab();
-//        Pages.clientDetailsPage().openAccountByNumber(safeDepositBoxAccount.getAccountNumber());
-//
-//        LOG.info("Step 4: Verify the displayed fields in view mode");
-//        // All the account fields are displayed showing correct values
-
-
+        LOG.info("Step 4: Verify the displayed fields in view mode");
+        Assert.assertEquals(safeDepositBoxAccount.getProductType(), Pages.accountDetailsPage().getProductTypeValue(), "Product type is not relevant");
+        Assert.assertEquals(safeDepositBoxAccount.getBoxSize(), Pages.accountDetailsPage().getBoxSizeValue(), "Box size is not relevant");
+        Assert.assertEquals(safeDepositBoxAccount.getAccountNumber(), Pages.accountDetailsPage().getAccountNumberValue(), "Account number is not relevant");
+        Assert.assertEquals(safeDepositBoxAccount.getAccountTitle(), Pages.accountDetailsPage().getAccountTitleValue(), "Account title is not relevant");
+        Assert.assertEquals(safeDepositBoxAccount.getBankBranch(), Pages.accountDetailsPage().getBankBranchValue(), "Bank branch is not relevant");
     }
 
 }
