212d536879475b2cf6a2f05eb6eeea0947d9cda0
dbarsukov <dbarsukov@nymbus.com>
2020-03-19 13:38:00 +0200

    Staged changes made for C22530 test case after switching to new framework.

--- src/test/java/com/nymbus/frontoffice/clientsearch/C22530_SearchByLastFourOfAccountNumberTest.java
+++ src/test/java/com/nymbus/frontoffice/clientsearch/C22530_SearchByLastFourOfAccountNumberTest.java
@@ -1,7 +1,7 @@
 package com.nymbus.frontoffice.clientsearch;
 
-import com.codeborne.selenide.Selenide;
 import com.nymbus.actions.Actions;
+import com.nymbus.actions.client.ClientsActions;
 import com.nymbus.core.base.BaseTest;
 import com.nymbus.core.utils.Constants;
 import com.nymbus.models.client.Client;
@@ -14,35 +14,42 @@ import org.testng.annotations.Test;
 @Epic("Frontoffice")
 @Feature("Clients search")
 @Owner("Dmytro")
-public class C22530_SearchByLastFourOfAccountNumber extends BaseTest {
+public class C22530_SearchByLastFourOfAccountNumberTest extends BaseTest {
 
     private Client client;
 
     @BeforeMethod
     public void preCondition() {
+
+        // Set up Client
         client = new Client().setDefaultClientData();
+        client.setClientStatus("Member");
+        client.setClientType("Individual");
         client.setAccountNumber("28461564083");
+
+        // Login to the system and create a client
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
+        ClientsActions.createClient().createClient(client);
+        Actions.loginActions().doLogOut();
     }
 
     @Test
     @Severity(SeverityLevel.CRITICAL)
     public void searchByLastFourOfAccountNumber() {
 
-//        LOG.info("Step 1: Log in to the system as the User from the precondition");
-        Selenide.open(Constants.URL);
+        logInfo("Step 1: Log in to the system as the User from the precondition");
         Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
 
-//        LOG.info("Step 2: Click within search field and try to search for an existing account (by 4 last digits of account number)");
+        logInfo("Step 2: Click within search field and try to search for an existing account (by 4 last digits of account number)");
         final String accountNumberQuery = client.getAccountNumber().substring(client.getAccountNumber().length() - 4);
         Pages.clientsPage().typeToClientsSearchInputField(accountNumberQuery);
-
         Assert.assertTrue(Pages.clientsPage().getAllLookupResults().size() > 0, "There are no relevant lookup results");
         if (Pages.clientsPage().getAllLookupResults().size() == 8) {
             Assert.assertTrue(Pages.clientsPage().isLoadMoreResultsButtonVisible(), "'Load more results' button is not visible in search lookup list");
         }
         Assert.assertTrue(Pages.clientsPage().isSearchResultsRelative(Pages.clientsPage().getAllLookupResults(), accountNumberQuery), "Search results are not relevant");
 
-//        LOG.info("Step 3: Click the 'Search' button");
+        logInfo("Step 3: Click the 'Search' button");
         Pages.clientsPage().clickOnSearchButton();
         Assert.assertTrue(Pages.clientsPage().isSearchResultsRelative(Pages.clientsSearchResultsPage().getAccountNumbersFromSearchResults(), accountNumberQuery));
     }
