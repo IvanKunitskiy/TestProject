e868f55a7549e78081b54b13d90d472b084b1334
pkyriushkin <pkyriushkin@nymbus.com>
2020-04-15 12:24:13 +0300

    NBTDB-832: Added C22558_EditTransferTest test class

---
+++ src/test/java/com/nymbus/frontoffice/clientsmanagement/C22558_EditTransferTest.java
@@ -0,0 +1,23 @@
+package com.nymbus.frontoffice.clientsmanagement;
+
+import com.nymbus.core.base.BaseTest;
+import io.qameta.allure.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("Clients Management")
+@Owner("Petro")
+public class C22558_EditTransferTest extends BaseTest {
+
+    @BeforeMethod
+    public void preCondition() {
+
+    }
+
+    @Test(description = "C22558, Edit transfer")
+    @Severity(SeverityLevel.CRITICAL)
+    public void editTransfer() {
+
+    }
+}
