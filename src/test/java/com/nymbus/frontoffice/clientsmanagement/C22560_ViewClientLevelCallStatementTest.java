d12f8d68ab4786401303cec92a002df05958032c
pkyriushkin <pkyriushkin@nymbus.com>
2020-04-17 13:17:39 +0300

    NBTDB-832: C22560 initial commit

---
+++ src/test/java/com/nymbus/frontoffice/clientsmanagement/C22560_ViewClientLevelCallStatementTest.java
@@ -0,0 +1,24 @@
+package com.nymbus.frontoffice.clientsmanagement;
+
+import com.nymbus.core.base.BaseTest;
+import io.qameta.allure.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("Clients Management")
+@Owner("Petro")
+public class C22560_ViewClientLevelCallStatementTest extends BaseTest {
+
+        @BeforeMethod
+        public void preCondition() {
+
+        }
+
+        @Test(description = "C22560, View client level 'Call Statement'")
+        @Severity(SeverityLevel.CRITICAL)
+        public void viewClientLevelCallStatement() {
+
+        }
+
+}
