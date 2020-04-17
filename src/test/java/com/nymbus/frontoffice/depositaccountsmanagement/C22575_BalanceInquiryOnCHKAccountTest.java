b2072f3cd6083141226f6458002524d245f72b53
pkyriushkin <pkyriushkin@nymbus.com>
2020-04-17 17:34:44 +0300

    NBTDB-832: Staged changes for C22575 test case

--- src/test/java/com/nymbus/frontoffice/depositaccountsmanagement/C22575_BalanceInquiryOnCHKAccountTest.java
+++ src/test/java/com/nymbus/frontoffice/depositaccountsmanagement/C22575_BalanceInquiryOnCHKAccountTest.java
@@ -1,8 +1,14 @@
 package com.nymbus.frontoffice.depositaccountsmanagement;
 
+import com.codeborne.selenide.Selenide;
+import com.nymbus.actions.Actions;
+import com.nymbus.actions.account.AccountActions;
+import com.nymbus.actions.client.ClientsActions;
 import com.nymbus.core.base.BaseTest;
+import com.nymbus.core.utils.Constants;
 import com.nymbus.models.client.Client;
 import com.nymbus.newmodels.account.Account;
+import com.nymbus.pages.Pages;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import org.testng.annotations.BeforeMethod;
@@ -16,11 +22,48 @@ public class C22575_BalanceInquiryOnCHKAccountTest extends BaseTest {
     @BeforeMethod
     public void preCondition() {
 
+        // Set up client
+        client = new Client().setDefaultClientData();
+        client.setClientStatus("Member");
+        client.setClientType("Individual");
+
+        // Set up account
+        chkAccount = new Account().setCHKAccountData();
+
+        // Create a client
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
+        ClientsActions.createClient().createClient(client);
+        client.setClientID(Pages.clientDetailsPage().getClientID());
+        AccountActions.createAccount().createCHKAccount(chkAccount);
+        Actions.tellerActions().assignAmountToAccount(chkAccount, "2", "20000");
+        Actions.loginActions().doLogOut();
+
     }
 
     @Test(description = "C22575, 'Balance inquiry' on CHK account")
     @Severity(SeverityLevel.CRITICAL)
     public void viewClientLevelCallStatement() {
+
+        logInfo("Step 1: Log in to the system as User from the preconditions");
+        Selenide.open(Constants.URL);
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
+
+        logInfo("Step 2: Search for the CHK account from the precondition and open it on Details");
+        Actions.clientPageActions().searchAndOpenClientByID(client);
+
+        logInfo("Step 3: Click [Balance Inquiry] button");
+        Pages.accountDetailsPage().clickBalanceInquiry();
+
+        logInfo("Step 4: Pay attention to the Available Balance and Current Balance values");
+        // TODO: parse receipt
+
+        logInfo("Step 5: Click [Print] button");
+
+
+        logInfo("Step 6: Open Balance Inquiry popup again");
+
+
+        logInfo("Step 7: Click [Close] button");
 
     }
 }
