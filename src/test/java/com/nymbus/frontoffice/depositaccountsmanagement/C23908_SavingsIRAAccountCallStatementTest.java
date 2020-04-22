30989f398e0e0517dc27c3f2d274679eecd1c7d3
pkyriushkin <pkyriushkin@nymbus.com>
2020-04-22 14:07:19 +0300

    NBTDB-832: Added a new test class and completed C23908 TC implementation.

---
+++ src/test/java/com/nymbus/frontoffice/depositaccountsmanagement/C23908_SavingsIRAAccountCallStatementTest.java
@@ -0,0 +1,65 @@
+package com.nymbus.frontoffice.depositaccountsmanagement;
+
+import com.codeborne.selenide.Selenide;
+import com.nymbus.actions.Actions;
+import com.nymbus.actions.account.AccountActions;
+import com.nymbus.actions.client.ClientsActions;
+import com.nymbus.core.base.BaseTest;
+import com.nymbus.core.utils.Constants;
+import com.nymbus.models.client.Client;
+import com.nymbus.newmodels.account.Account;
+import com.nymbus.pages.Pages;
+import io.qameta.allure.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("Deposit Accounts Management")
+@Owner("Petro")
+public class C23908_SavingsIRAAccountCallStatementTest extends BaseTest {
+
+    private Client client;
+    private Account iraAccount;
+
+    @BeforeMethod
+    public void preCondition() {
+
+        // Set up client
+        client = new Client().setDefaultClientData();
+        client.setClientStatus("Member");
+        client.setClientType("Individual");
+
+        // Set up account
+        iraAccount = new Account().setIRAAccountData();
+
+        // Create a client
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
+        ClientsActions.createClient().createClient(client);
+
+        // Create account
+        AccountActions.createAccount().createCHKAccount(iraAccount);
+
+        // Create transaction
+        Actions.tellerActions().assignAmountToAccount(iraAccount, "2", "20000");
+        Actions.loginActions().doLogOut();
+    }
+
+    @Test(description = "C23908, Savings IRA account call statement")
+    @Severity(SeverityLevel.CRITICAL)
+    public void savingsIRAAccountCallStatement() {
+
+        logInfo("Step 1: Log in to the system as User from the preconditions");
+        Selenide.open(Constants.URL);
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
+
+        logInfo("Step 2: Search for the CHK account from the precondition and open it on Transactions tab");
+        Actions.clientPageActions().searchAndOpenAccountByAccountNumber(iraAccount);
+        Pages.accountNavigationPage().clickTransactionsTab();
+
+        logInfo("Step 3: Click [Call Statement] button");
+        Pages.transactionsPage().clickTheCallStatementButton();
+
+        logInfo("Step 4: Look through the CHK Call Statement data and verify it contains correct data");
+        // TODO: parse statement file
+    }
+}
