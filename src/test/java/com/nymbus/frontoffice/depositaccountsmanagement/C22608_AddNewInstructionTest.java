7b85a48e5744017206f574b0a22c91a356ac3662
pkyriushkin <pkyriushkin@nymbus.com>
2020-04-29 14:18:15 +0300

    NBTDB-832: C22608, Initial commit. Added a test class.

---
+++ src/test/java/com/nymbus/frontoffice/depositaccountsmanagement/C22608_AddNewInstructionTest.java
@@ -0,0 +1,23 @@
+package com.nymbus.frontoffice.depositaccountsmanagement;
+
+import com.nymbus.core.base.BaseTest;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class C22608_AddNewInstructionTest extends BaseTest {
+
+
+
+    @BeforeMethod
+    public void preCondition() {
+
+    }
+
+    @Test(description = "C22608, Add new instruction")
+    @Severity(SeverityLevel.CRITICAL)
+    public void addNewInstruction() {
+
+    }
+}
