eb5167cfcabec466cf67a945082f9358f6a68d6a
pkyriushkin <pkyriushkin@nymbus.com>
2020-04-29 12:34:42 +0300

    NBTDB-832: C22606, Initial. Added test class.

---
+++ src/test/java/com/nymbus/frontoffice/depositaccountsmanagement/C22606_AssignAccountLevelNotesToUserTest.java
@@ -0,0 +1,24 @@
+package com.nymbus.frontoffice.depositaccountsmanagement;
+
+import com.nymbus.core.base.BaseTest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("Deposit Accounts Management")
+@Owner("Petro")
+public class C22606_AssignAccountLevelNotesToUserTest extends BaseTest {
+
+    @BeforeMethod
+    public void preCondition() {
+
+    }
+
+    @Test(description = "C22606, Assign account level notes to user")
+    public void assignAccountLevelNotesToUser() {
+
+    }
+}
