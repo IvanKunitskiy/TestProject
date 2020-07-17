48bcb260122dac86233593beb10ff38e896fca98
dbarsukov <dbarsukov@nymbus.com>
2020-07-17 15:09:13 +0300

    NBTDB-832: add img src validation for C22557_ViewEditDeleteRestoreClientLevelDocumentationTest

--- src/main/java/com/nymbus/pages/clients/documents/DocumentOverviewPage.java
+++ src/main/java/com/nymbus/pages/clients/documents/DocumentOverviewPage.java
@@ -5,6 +5,7 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 public class DocumentOverviewPage extends PageTools {
+    private By documentImage = By.xpath("//*[@id='main-content']//img");
     private By closeButton = By.xpath("//button[@data-test-id='action-close']");
 
     @Step("Click the 'Close' button")
@@ -12,5 +13,12 @@ public class DocumentOverviewPage extends PageTools {
         waitForElementVisibility(closeButton);
         waitForElementClickable(closeButton);
         click(closeButton);
+    }
+
+    @Step("Get image src")
+    public String getImageSrc() {
+        waitForElementVisibility(documentImage);
+        waitForElementClickable(documentImage);
+        return getWebElement(documentImage).getAttribute("src");
     }
 }
