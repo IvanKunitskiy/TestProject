676881819081e41871ca1f131cf2d6f1d74e852e
dbarsukov <dbarsukov@nymbus.com>
2020-07-22 17:58:18 +0300

    NBTDB-832: add C25367_108ATMDepositForeignTest

---
+++ src/test/java/com/nymbus/frontoffice/transactions/C25367_108ATMDepositForeignTest.java
@@ -0,0 +1,137 @@
+package com.nymbus.frontoffice.transactions;
+
+import com.nymbus.actions.Actions;
+import com.nymbus.actions.account.AccountActions;
+import com.nymbus.actions.client.ClientsActions;
+import com.nymbus.core.base.BaseTest;
+import com.nymbus.core.utils.Constants;
+import com.nymbus.core.utils.DateTime;
+import com.nymbus.newmodels.account.Account;
+import com.nymbus.newmodels.client.IndividualClient;
+import com.nymbus.newmodels.client.other.debitcard.DebitCard;
+import com.nymbus.newmodels.generation.bincontrol.BinControlConstructor;
+import com.nymbus.newmodels.generation.bincontrol.builder.BinControlBuilder;
+import com.nymbus.newmodels.generation.client.builder.IndividualClientBuilder;
+import com.nymbus.newmodels.generation.client.builder.type.individual.IndividualBuilder;
+import com.nymbus.newmodels.generation.debitcard.DebitCardConstructor;
+import com.nymbus.newmodels.generation.debitcard.builder.DebitCardBuilder;
+import com.nymbus.newmodels.settings.bincontrol.BinControl;
+import com.nymbus.newmodels.transaction.verifyingModels.BalanceDataForCHKAcc;
+import com.nymbus.newmodels.transaction.verifyingModels.NonTellerTransactionData;
+import com.nymbus.newmodels.transaction.verifyingModels.TransactionData;
+import com.nymbus.pages.Pages;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class C25367_108ATMDepositForeignTest extends BaseTest {
+    private BalanceDataForCHKAcc expectedBalanceData;
+    private NonTellerTransactionData nonTellerTransactionData;
+    private TransactionData chkAccTransactionData;
+    private String checkingAccountNumber;
+    private double transactionAmount;
+    private IndividualClient client;
+    private String foreignTerminalID = "3456453523";
+    private double foreignFeeValue;
+
+    @BeforeMethod
+    public void prepareTransactionData() {
+        // Set up Client, Account
+        IndividualClientBuilder individualClientBuilder =  new IndividualClientBuilder();
+        individualClientBuilder.setIndividualClientBuilder(new IndividualBuilder());
+        client = individualClientBuilder.buildClient();
+        Account checkAccount = new Account().setCHKAccountData();
+        transactionAmount = 100.00;
+
+        // Set up debit card and bin control
+        DebitCardConstructor debitCardConstructor = new DebitCardConstructor();
+        DebitCardBuilder debitCardBuilder = new DebitCardBuilder();
+        debitCardConstructor.constructDebitCard(debitCardBuilder);
+        DebitCard debitCard = debitCardBuilder.getCard();
+
+        BinControlConstructor binControlConstructor = new BinControlConstructor();
+        BinControlBuilder binControlBuilder = new BinControlBuilder();
+        binControlConstructor.constructBinControl(binControlBuilder);
+        BinControl binControl = binControlBuilder.getBinControl();
+        binControl.setBinNumber("510986");
+        binControl.setCardDescription("Consumer Debit");
+
+        Actions.debitCardModalWindowActions().setDebitCardWithBinControl(debitCard, binControl);
+        debitCard.getAccounts().add(checkAccount.getAccountNumber());
+        debitCard.setNameOnCard(client.getNameForDebitCard());
+
+        // Log in and create client
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
+        foreignFeeValue = Actions.nonTellerTransactionActions().getForeignFee(1);
+
+        ClientsActions.individualClientActions().createClient(client);
+        ClientsActions.individualClientActions().setClientDetailsData(client);
+        ClientsActions.individualClientActions().setDocumentation(client);
+
+        // Create CHK account
+        Actions.clientPageActions().searchAndOpenClientByName(client.getInitials());
+        AccountActions.createAccount().createCHKAccountForTransactionPurpose(checkAccount);
+        checkingAccountNumber = checkAccount.getAccountNumber();
+
+        //Create debit card
+        Actions.clientPageActions().searchAndOpenClientByName(client.getInitials());
+        Pages.clientDetailsPage().clickOnMaintenanceTab();
+        Pages.clientDetailsPage().clickOnNewDebitCardButton();
+        Actions.debitCardModalWindowActions().fillDebitCard(debitCard);
+        String expirationDate = Actions.debitCardModalWindowActions().getExpirationDate();
+        Pages.debitCardModalWindow().clickOnSaveAndFinishButton();
+        Pages.debitCardModalWindow().waitForAddNewDebitCardModalWindowInvisibility();
+        String debitCardNumber = Actions.debitCardModalWindowActions().getCardNumber(1);
+
+        Actions.clientPageActions().searchAndOpenClientByName(checkAccount.getAccountNumber());
+        expectedBalanceData = AccountActions.retrievingAccountData().getBalanceDataForCHKAcc();
+
+        // Set up nonTeller transaction data
+        nonTellerTransactionData = new NonTellerTransactionData();
+        nonTellerTransactionData.setCardNumber(debitCardNumber);
+        nonTellerTransactionData.setAmount(transactionAmount);
+        nonTellerTransactionData.setExpirationDate(expirationDate);
+        nonTellerTransactionData.setTerminalId(foreignTerminalID);
+        chkAccTransactionData = new TransactionData(DateTime.getLocalDateOfPattern("MM/dd/yyyy"), DateTime.getLocalDateOfPattern("MM/dd/yyyy"),
+                "+", expectedBalanceData.getCurrentBalance(),
+                transactionAmount);
+
+        Actions.loginActions().doLogOut();
+    }
+
+    @Test(description = " C25367, 108 ATM Deposit FOREIGN")
+    @Severity(SeverityLevel.CRITICAL)
+    public void verify108ATMDepositFOREIGNTransaction() {
+        logInfo("Step 1: Go to the Swagger and log in as the User from the preconditions");
+        logInfo("Step 2:Expand widgets-controller/widget._GenericProcess and run the following request with ONUS terminal ID");
+        Actions.nonTellerTransactionActions().performATMWDepositONUSTransaction(nonTellerTransactionData);
+
+        logInfo("Step 3: Log in to the system as the User from the preconditions");
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
+
+        logInfo("Step 4: Search for CHK account from the precondition and open it on Instructions tab");
+        Actions.clientPageActions().searchAndOpenClientByName(checkingAccountNumber);
+
+        logInfo("Step 5: Check the list of instructions for the account (if exist) and delete the Hold with type Reg CC");
+        AccountActions.editAccount().goToInstructionsTab();
+        String INSTRUCTION_REASON = "Reg CC";
+        AccountActions.createInstruction().deleteInstructionByReasonText(INSTRUCTION_REASON);
+
+        logInfo("Step 6: Verify Account's: \n" +
+                "- current balance \n" +
+                "- available balance \n" +
+                "- Transactions history");
+        AccountActions.editAccount().goToDetailsTab();
+        expectedBalanceData.addAmount(transactionAmount);
+        BalanceDataForCHKAcc actualBalanceData = AccountActions.retrievingAccountData().getBalanceDataForCHKAcc();
+        Assert.assertEquals(actualBalanceData, expectedBalanceData, "CHKAccount balances is not correct!");
+
+        chkAccTransactionData.setBalance(expectedBalanceData.getCurrentBalance());
+        AccountActions.retrievingAccountData().goToTransactionsTab();
+        int offset = AccountActions.retrievingAccountData().getOffset();
+        TransactionData actualTransactionData = AccountActions.retrievingAccountData().getTransactionDataWithOffset(offset);
+        Assert.assertEquals(actualTransactionData, chkAccTransactionData, "Transaction data doesn't match!");
+    }
+}
