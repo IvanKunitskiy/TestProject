fbc64f6edcc994bcc8e32df32504a22262e40ced
dbarsukov <dbarsukov@nymbus.com>
2020-08-25 18:39:19 +0300

    NBTDB-832: add C22769_223DebitPurchaseTest

---
+++ src/test/java/com/nymbus/frontoffice/transactions/C22769_223DebitPurchaseTest.java
@@ -0,0 +1,150 @@
+package com.nymbus.frontoffice.transactions;
+
+import com.nymbus.actions.Actions;
+import com.nymbus.actions.account.AccountActions;
+import com.nymbus.actions.client.ClientsActions;
+import com.nymbus.actions.webadmin.WebAdminActions;
+import com.nymbus.core.base.BaseTest;
+import com.nymbus.core.utils.Constants;
+import com.nymbus.core.utils.DateTime;
+import com.nymbus.core.utils.Generator;
+import com.nymbus.newmodels.account.Account;
+import com.nymbus.newmodels.client.IndividualClient;
+import com.nymbus.newmodels.client.other.debitcard.DebitCard;
+import com.nymbus.newmodels.generation.bincontrol.BinControlConstructor;
+import com.nymbus.newmodels.generation.bincontrol.builder.BinControlBuilder;
+import com.nymbus.newmodels.generation.client.builder.IndividualClientBuilder;
+import com.nymbus.newmodels.generation.client.builder.type.individual.IndividualBuilder;
+import com.nymbus.newmodels.generation.debitcard.DebitCardConstructor;
+import com.nymbus.newmodels.generation.debitcard.builder.DebitCardBuilder;
+import com.nymbus.newmodels.generation.tansactions.TransactionConstructor;
+import com.nymbus.newmodels.generation.tansactions.builder.GLDebitMiscCreditBuilder;
+import com.nymbus.newmodels.settings.bincontrol.BinControl;
+import com.nymbus.newmodels.transaction.Transaction;
+import com.nymbus.newmodels.transaction.enums.TransactionCode;
+import com.nymbus.newmodels.transaction.verifyingModels.BalanceData;
+import com.nymbus.newmodels.transaction.verifyingModels.NonTellerTransactionData;
+import com.nymbus.newmodels.transaction.verifyingModels.TransactionData;
+import com.nymbus.pages.Pages;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class C22769_223DebitPurchaseTest extends BaseTest {
+    private BalanceData expectedBalanceData;
+    private TransactionData savingAccTransactionData;
+    private NonTellerTransactionData nonTellerTransactionData;
+    private String savingAccountNumber;
+    private double transactionAmount;
+    private IndividualClient client;
+
+    @BeforeMethod
+    public void prepareTransactionData() {
+        // Set up Client, Account and Transaction
+        IndividualClientBuilder individualClientBuilder =  new IndividualClientBuilder();
+        individualClientBuilder.setIndividualClientBuilder(new IndividualBuilder());
+        client = individualClientBuilder.buildClient();
+        Account savingsAccount = new Account().setSavingsAccountData();
+        Transaction glDebitMiscCreditTransaction = new TransactionConstructor(new GLDebitMiscCreditBuilder()).constructTransaction();
+
+        // Init nonTellerTransactionData
+        nonTellerTransactionData = new NonTellerTransactionData();
+
+        // Set up debit card and bin control
+        DebitCardConstructor debitCardConstructor = new DebitCardConstructor();
+        DebitCardBuilder debitCardBuilder = new DebitCardBuilder();
+        debitCardConstructor.constructDebitCard(debitCardBuilder);
+        DebitCard debitCard = debitCardBuilder.getCard();
+
+        BinControlConstructor binControlConstructor = new BinControlConstructor();
+        BinControlBuilder binControlBuilder = new BinControlBuilder();
+        binControlConstructor.constructBinControl(binControlBuilder);
+        BinControl binControl = binControlBuilder.getBinControl();
+        binControl.setBinNumber("510986");
+        binControl.setCardDescription("Consumer Debit");
+
+        Actions.debitCardModalWindowActions().setDebitCardWithBinControl(debitCard, binControl);
+        debitCard.getAccounts().add(savingsAccount.getAccountNumber());
+        debitCard.setNameOnCard(client.getNameForDebitCard());
+
+        // Log in and create client
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
+
+        ClientsActions.individualClientActions().createClient(client);
+        ClientsActions.individualClientActions().setClientDetailsData(client);
+        ClientsActions.individualClientActions().setDocumentation(client);
+
+        // Create Savings  account
+        Actions.clientPageActions().searchAndOpenClientByName(client.getInitials());
+        AccountActions.createAccount().createSavingAccountForTransactionPurpose(savingsAccount);
+        savingAccountNumber = savingsAccount.getAccountNumber();
+
+        // Create debit card for saving acc
+        createDebitCard(client.getInitials(), debitCard);
+        Actions.debitCardModalWindowActions().setExpirationDateAndCardNumber(nonTellerTransactionData, 1);
+
+        // Re-login in system for updating teller session
+        Actions.loginActions().doLogOut();
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
+
+        // Perform transaction
+        Actions.transactionActions().loginTeller();
+        Actions.transactionActions().goToTellerPage();
+        Actions.transactionActions().createGlDebitMiscCreditTransaction(glDebitMiscCreditTransaction);
+        Actions.transactionActions().clickCommitButton();
+        Pages.tellerPage().closeModal();
+
+        Actions.loginActions().doLogOutProgrammatically();
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
+
+        transactionAmount = glDebitMiscCreditTransaction.getTransactionDestination().getAmount();
+        Actions.clientPageActions().searchAndOpenClientByName(savingAccountNumber);
+        expectedBalanceData = AccountActions.retrievingAccountData().getBalanceData();
+
+        // Set up nonTeller transaction data
+        nonTellerTransactionData.setAmount(transactionAmount);
+        savingAccTransactionData = new TransactionData(DateTime.getLocalDateOfPattern("MM/dd/yyyy"), DateTime.getLocalDateOfPattern("MM/dd/yyyy"),
+                "-", expectedBalanceData.getCurrentBalance(), transactionAmount);
+
+        Actions.loginActions().doLogOut();
+    }
+
+    @Test(description = " C22769, 223 - Debit Purchase")
+    @Severity(SeverityLevel.CRITICAL)
+    public void verify223DebitPurchaseTest() {
+        logInfo("Step 1: Go to the Swagger and log in as the User from the preconditions");
+        logInfo("Step 2: Expand widgets-controller/widget._GenericProcess and run the following request:");
+        Actions.nonTellerTransactionActions().performATMTransaction(getFieldsMap(nonTellerTransactionData));
+
+        String transcode = TransactionCode.ATM_DEBIT_PURCHASE_223.getTransCode().split("\\s+")[0];
+        WebAdminActions.webAdminTransactionActions().setTransactionPostDateAndEffectiveDate(savingAccTransactionData, savingAccountNumber, transcode);
+    }
+
+    private void createDebitCard(String clientInitials, DebitCard debitCard) {
+        Actions.clientPageActions().searchAndOpenClientByName(clientInitials);
+        Pages.clientDetailsPage().clickOnMaintenanceTab();
+        Pages.clientDetailsPage().clickOnNewDebitCardButton();
+        Actions.debitCardModalWindowActions().fillDebitCard(debitCard);
+        Pages.debitCardModalWindow().clickOnSaveAndFinishButton();
+        Pages.debitCardModalWindow().waitForAddNewDebitCardModalWindowInvisibility();
+    }
+
+    private Map<String, String> getFieldsMap(NonTellerTransactionData transactionData) {
+        Map<String, String > result = new HashMap<>();
+        result.put("0", "0200");
+        result.put("3", "001000");
+        result.put("4", transactionData.getAmount());
+        result.put("11", String.valueOf(Generator.genInt(100000000, 922337203)));
+        result.put("18", "6010");
+        result.put("22", "801");
+        result.put("35", String.format("%s=%s", transactionData.getCardNumber(), transactionData.getExpirationDate()));
+        result.put("43", "Long ave. bld. 34      Nashville      US");
+        result.put("58", "0000000002U");
+
+        return  result;
+    }
+}
