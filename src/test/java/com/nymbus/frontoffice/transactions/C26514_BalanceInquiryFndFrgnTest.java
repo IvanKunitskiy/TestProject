e8c2d96bba19f2e698445dcdb85fc4f1e23cc623
pkyriushkin <pkyriushkin@nymbus.com>
2020-08-28 15:41:50 +0300

    NBTDB-832: Added new test class for C26514_BalanceInquiryFndFrgnTest

---
+++ src/test/java/com/nymbus/frontoffice/transactions/C26514_BalanceInquiryFndFrgnTest.java
@@ -0,0 +1,23 @@
+package com.nymbus.frontoffice.transactions;
+
+import com.nymbus.core.base.BaseTest;
+import io.qameta.allure.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("Transactions")
+@Owner("Petro")
+public class C26514_BalanceInquiryFndFrgnTest extends BaseTest {
+
+    @BeforeMethod
+    public void preCondition() {
+
+    }
+
+    @Test
+    @Severity(SeverityLevel.CRITICAL)
+    public void balanceInquiryFndFrgn() {
+
+    }
+}
