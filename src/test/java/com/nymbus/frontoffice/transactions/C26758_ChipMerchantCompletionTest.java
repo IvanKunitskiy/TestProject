21cc21ce9c801bc7ab5abb0475338c4d88524490
pkyriushkin <pkyriushkin@nymbus.com>
2020-09-04 12:17:48 +0300

    NBTDB-832: Added test class for C26758

---
+++ src/test/java/com/nymbus/frontoffice/transactions/C26758_ChipMerchantCompletionTest.java
@@ -0,0 +1,18 @@
+package com.nymbus.frontoffice.transactions;
+
+import com.nymbus.core.base.BaseTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class C26758_ChipMerchantCompletionTest extends BaseTest {
+
+    @BeforeMethod
+    public void preCondition() {
+
+    }
+
+    @Test
+    public void chipMerchantCompletionTest() {
+
+    }
+}
