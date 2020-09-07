8ef5afde7c9a7f3d480215475b83e3f773f9f7cf
pkyriushkin <pkyriushkin@nymbus.com>
2020-09-07 13:17:28 +0300

    NBTDB-832: Completed C26758_ChipMerchantCompletionTest TC.

--- src/test/java/com/nymbus/frontoffice/transactions/C26758_ChipMerchantCompletionTest.java
+++ src/test/java/com/nymbus/frontoffice/transactions/C26758_ChipMerchantCompletionTest.java
@@ -3,8 +3,10 @@ package com.nymbus.frontoffice.transactions;
 import com.nymbus.actions.Actions;
 import com.nymbus.actions.account.AccountActions;
 import com.nymbus.actions.client.ClientsActions;
+import com.nymbus.actions.webadmin.WebAdminActions;
 import com.nymbus.core.base.BaseTest;
 import com.nymbus.core.utils.Constants;
+import com.nymbus.core.utils.DateTime;
 import com.nymbus.newmodels.account.Account;
 import com.nymbus.newmodels.client.IndividualClient;
 import com.nymbus.newmodels.client.other.debitcard.DebitCard;
@@ -19,9 +21,12 @@ import com.nymbus.newmodels.generation.tansactions.TransactionConstructor;
 import com.nymbus.newmodels.generation.tansactions.builder.GLDebitMiscCreditBuilder;
 import com.nymbus.newmodels.settings.bincontrol.BinControl;
 import com.nymbus.newmodels.transaction.Transaction;
+import com.nymbus.newmodels.transaction.enums.TransactionCode;
 import com.nymbus.newmodels.transaction.verifyingModels.BalanceDataForCHKAcc;
 import com.nymbus.newmodels.transaction.verifyingModels.NonTellerTransactionData;
+import com.nymbus.newmodels.transaction.verifyingModels.TransactionData;
 import com.nymbus.pages.Pages;
+import io.qameta.allure.*;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -29,12 +34,16 @@ import org.testng.annotations.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+@Epic("Frontoffice")
+@Feature("Transactions")
+@Owner("Petro")
 public class C26758_ChipMerchantCompletionTest extends BaseTest {
 
     private IndividualClient client;
     private String chkAccountNumber;
     private BalanceDataForCHKAcc expectedBalanceDataForCheckingAcc;
     private NonTellerTransactionData chipMerchantCompletionTransactionData;
+    private TransactionData chkAccTransactionData;
     private final double requestTransactionAmount = 21.90;
 
     @BeforeMethod
@@ -54,10 +63,11 @@ public class C26758_ChipMerchantCompletionTest extends BaseTest {
         glDebitMiscCreditTransaction.getTransactionDestination().setAccountNumber(chkAccountNumber);
         glDebitMiscCreditTransaction.getTransactionDestination().setTransactionCode("109 - Deposit");
 
-        // Set up nonTeller transaction data
+        // Set up nonTeller transaction data for 'Chip Merchant Auth'
         NonTellerTransactionData chipMerchantAuthTransactionData = new NonTellerTransactionData();
         chipMerchantAuthTransactionData.setAmount(requestTransactionAmount);
 
+        // Set up nonTeller transaction data for 'Chip Merchant Completion'
         chipMerchantCompletionTransactionData = new NonTellerTransactionData();
         chipMerchantCompletionTransactionData.setAmount(requestTransactionAmount);
 
@@ -94,6 +104,7 @@ public class C26758_ChipMerchantCompletionTest extends BaseTest {
         // Create debit card for CHK acc
         createDebitCard(client.getInitials(), debitCard);
         Actions.debitCardModalWindowActions().setExpirationDateAndCardNumber(chipMerchantCompletionTransactionData, 1);
+        Pages.cardsManagementPage().clickBackToMaintenanceButton();
         Actions.debitCardModalWindowActions().setExpirationDateAndCardNumber(chipMerchantAuthTransactionData, 1);
 
         // Re-login in system for updating teller session and capture account balances
@@ -106,25 +117,31 @@ public class C26758_ChipMerchantCompletionTest extends BaseTest {
         Actions.transactionActions().createGlDebitMiscCreditTransaction(glDebitMiscCreditTransaction);
         Actions.transactionActions().clickCommitButton();
         Pages.tellerPage().closeModal();
-
         Actions.loginActions().doLogOutProgrammatically();
 
+        // Login, capture current and available balance of the account and logout
         Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
         Actions.clientPageActions().searchAndOpenClientByName(chkAccountNumber);
         expectedBalanceDataForCheckingAcc = AccountActions.retrievingAccountData().getBalanceDataForCHKAcc();
-
         Actions.loginActions().doLogOutProgrammatically();
 
+        // Perform 'Chip Merchant Auth' request
         String[] actions = {"0100"};
         Actions.nonTellerTransactionActions().performATMTransaction(getChipMerchantAuthFieldsMap(chipMerchantAuthTransactionData), actions);
+        // update the expected available balance
         expectedBalanceDataForCheckingAcc.reduceAvailableBalance(requestTransactionAmount);
+
+        chkAccTransactionData = new TransactionData(DateTime.getLocalDateOfPattern("MM/dd/yyyy"), DateTime.getLocalDateOfPattern("MM/dd/yyyy"),
+                "-", expectedBalanceDataForCheckingAcc.getAvailableBalance(), requestTransactionAmount);
     }
 
-    @Test
+    @Test(description = "C26758, Chip Merchant Completion")
+    @Severity(SeverityLevel.CRITICAL)
     public void chipMerchantCompletionTest() {
+
         String[] actions = {"0220"};
         Actions.nonTellerTransactionActions().performATMTransaction(getChipMerchantCompletionFieldsMap(chipMerchantCompletionTransactionData), actions);
-        expectedBalanceDataForCheckingAcc.reduceAvailableBalance(requestTransactionAmount);
+        expectedBalanceDataForCheckingAcc.reduceCurrentBalance(requestTransactionAmount);
 
         logInfo("Step 3: Log in to the system as the User from the preconditions");
         logInfo("Step 4: Search for CHK account from the precondition and verify its current and available balance");
@@ -136,23 +153,24 @@ public class C26758_ChipMerchantCompletionTest extends BaseTest {
         Assert.assertEquals(AccountActions.retrievingAccountData().getAvailableBalance(),
                 expectedBalanceDataForCheckingAcc.getAvailableBalance(), "CHK account available balance is not correct!");
 
-        // TODO: Committed ATM transaction is not present in Transactions tab as it is Pre-Auth transaction
         logInfo("Step 5: Open CHK account on Transactions tab and verify Transactions history.\n" +
                 "Make sure that there is no ATM transaction in Transactions tab");
+        String transcode = TransactionCode.DEBIT_PURCHASE_123.getTransCode().split("\\s+")[0];
+        WebAdminActions.webAdminTransactionActions().setTransactionPostDateAndEffectiveDate(chkAccTransactionData, chkAccountNumber, transcode);
         AccountActions.retrievingAccountData().goToTransactionsTab();
-        Assert.assertEquals(Pages.accountTransactionPage().getTransactionItemsCount(), 1,
-                "Transactions count on 'Transactions' tab is incorrect");
-        Assert.assertTrue(Actions.transactionActions().isTransactionCodePresent("109 - Deposit"),
-                "109 - Deposit transaction is not present in transaction list");
+        int offset = AccountActions.retrievingAccountData().getOffset();
+        TransactionData actualTransactionData = AccountActions.retrievingAccountData().getTransactionDataWithOffset(offset);
+
+        Assert.assertEquals(actualTransactionData, chkAccTransactionData, "Transaction data doesn't match!");
+        Assert.assertEquals(Pages.accountTransactionPage().getTransactionItemsCount(), 2,
+                "Transaction count is incorrect!");
+        Assert.assertTrue(Actions.transactionActions().isTransactionCodePresent(TransactionCode.DEBIT_PURCHASE_123.getTransCode(), offset),
+                "'123 - Debit Purchase' presents in transaction list");
 
         logInfo("Step 6: Open CHK account on Instructions tab and verify that there is a Hold with amount == transaction amount");
         AccountActions.editAccount().goToInstructionsTab();
-        Assert.assertEquals(Pages.accountInstructionsPage().getCreatedInstructionsCount(), 1,
-                "Instruction was created");
-        Pages.accountInstructionsPage().clickInstructionInListByIndex(1);
-        double holdInstructionAmount = AccountActions.retrievingAccountData().getInstructionAmount();
-        Assert.assertEquals(requestTransactionAmount, holdInstructionAmount,
-                "Hold instruction amount is not equal to reequest transaction amount!");
+        Assert.assertEquals(Pages.accountInstructionsPage().getCreatedInstructionsCount(), 0,
+                "Hold instruction was not created");
 
         logInfo("Step 7: Go to Client Maintenance and click [View all Cards] button in 'Cards Management' widget");
         logInfo("Step 8: Click [View History] link on the Debit Card from the precondition");
@@ -164,13 +182,12 @@ public class C26758_ChipMerchantCompletionTest extends BaseTest {
                 "'Transaction Reason Code' is not equal to '00 -- Approved or completed successfully'");
 
         String transactionDescription = Pages.cardsManagementPage().getTransactionDescription(1);
-        Assert.assertEquals(transactionDescription, "Pre-Authorization Request",
-                "Transaction description is not equal to 'Pre-Authorization Request'");
+        Assert.assertEquals(transactionDescription, "Purchase Completion",
+                "Transaction description is not equal to 'Purchase Completion'");
 
         String transactionAmount = Pages.cardsManagementPage().getTransactionAmount(1);
         Assert.assertEquals(Double.parseDouble(transactionAmount), requestTransactionAmount,
-                "Transaction description is not equal to 'Pre-Authorization Request'");
-
+                "'Purchase Completion' amount is not equal to transaction amount");
     }
 
     private void createDebitCard(String clientInitials, DebitCard debitCard) {
