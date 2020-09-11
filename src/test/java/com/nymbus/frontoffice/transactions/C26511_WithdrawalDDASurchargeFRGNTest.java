be9313e0366cde5663541d0aad9f3d576f3bffa2
dbarsukov <dbarsukov@nymbus.com>
2020-09-11 11:05:04 +0300

    NBTDB-832: add C26511_WithdrawalDDASurchargeFRGNTest

---
+++ src/test/java/com/nymbus/frontoffice/transactions/C26511_WithdrawalDDASurchargeFRGNTest.java
@@ -0,0 +1,140 @@
+package com.nymbus.frontoffice.transactions;
+
+import com.nymbus.actions.Actions;
+import com.nymbus.actions.account.AccountActions;
+import com.nymbus.actions.client.ClientsActions;
+import com.nymbus.core.base.BaseTest;
+import com.nymbus.core.utils.Constants;
+import com.nymbus.core.utils.Generator;
+import com.nymbus.newmodels.account.Account;
+import com.nymbus.newmodels.client.IndividualClient;
+import com.nymbus.newmodels.client.other.debitcard.DebitCard;
+import com.nymbus.newmodels.client.other.debitcard.types.TranslationTypeAllowed;
+import com.nymbus.newmodels.generation.bincontrol.BinControlConstructor;
+import com.nymbus.newmodels.generation.bincontrol.builder.BinControlBuilder;
+import com.nymbus.newmodels.generation.client.builder.IndividualClientBuilder;
+import com.nymbus.newmodels.generation.client.builder.type.individual.IndividualBuilder;
+import com.nymbus.newmodels.generation.debitcard.DebitCardConstructor;
+import com.nymbus.newmodels.generation.debitcard.builder.DebitCardBuilder;
+import com.nymbus.newmodels.generation.tansactions.TransactionConstructor;
+import com.nymbus.newmodels.generation.tansactions.builder.GLDebitMiscCreditCHKAccBuilder;
+import com.nymbus.newmodels.settings.bincontrol.BinControl;
+import com.nymbus.newmodels.transaction.Transaction;
+import com.nymbus.newmodels.transaction.verifyingModels.BalanceDataForCHKAcc;
+import com.nymbus.newmodels.transaction.verifyingModels.NonTellerTransactionData;
+import com.nymbus.pages.Pages;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class C26511_WithdrawalDDASurchargeFRGNTest extends BaseTest {
+    private IndividualClient client;
+    private String chkAccountNumber;
+    private NonTellerTransactionData nonTellerTransactionData;
+    private BalanceDataForCHKAcc expectedBalanceData;
+    private double transactionAmount = 100.00;
+
+    @BeforeMethod
+    public void preCondition() {
+        // Set up Client
+        IndividualClientBuilder individualClientBuilder =  new IndividualClientBuilder();
+        individualClientBuilder.setIndividualClientBuilder(new IndividualBuilder());
+        client = individualClientBuilder.buildClient();
+        Transaction glDebitMiscCreditTransaction = new TransactionConstructor(new GLDebitMiscCreditCHKAccBuilder()).constructTransaction();
+
+        // Set up CHK account
+        Account chkAccount = new Account().setCHKAccountData();
+        chkAccountNumber = chkAccount.getAccountNumber();
+        glDebitMiscCreditTransaction.getTransactionDestination().setAccountNumber(chkAccountNumber);
+
+        // Set up nonTeller transaction data
+        nonTellerTransactionData = new NonTellerTransactionData();
+        nonTellerTransactionData.setAmount(transactionAmount);
+
+        // Set up debit card and bin control
+        DebitCardConstructor debitCardConstructor = new DebitCardConstructor();
+        DebitCardBuilder debitCardBuilder = new DebitCardBuilder();
+        debitCardConstructor.constructDebitCard(debitCardBuilder);
+        DebitCard debitCard = debitCardBuilder.getCard();
+        debitCard.setTranslationTypeAllowed(TranslationTypeAllowed.BOTH_PIN_AND_SIGNATURE);
+
+        BinControlConstructor binControlConstructor = new BinControlConstructor();
+        BinControlBuilder binControlBuilder = new BinControlBuilder();
+        binControlConstructor.constructBinControl(binControlBuilder);
+        BinControl binControl = binControlBuilder.getBinControl();
+        binControl.setBinNumber("510986");
+        binControl.setCardDescription("Consumer Debit");
+
+        Actions.debitCardModalWindowActions().setDebitCardWithBinControl(debitCard, binControl);
+        debitCard.getAccounts().add(chkAccount.getAccountNumber());
+        debitCard.setNameOnCard(client.getNameForDebitCard());
+
+        // Log in
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
+
+        // Create client
+        ClientsActions.individualClientActions().createClient(client);
+        ClientsActions.individualClientActions().setClientDetailsData(client);
+        ClientsActions.individualClientActions().setDocumentation(client);
+
+        // Create CHK account
+        Actions.clientPageActions().searchAndOpenClientByName(client.getInitials());
+        AccountActions.createAccount().createCHKAccountForTransactionPurpose(chkAccount);
+
+        // Create debit card for checking acc
+        createDebitCard(client.getInitials(), debitCard);
+        Actions.debitCardModalWindowActions().setExpirationDateAndCardNumber(nonTellerTransactionData, 1);
+
+        // Re-login in system for updating teller session
+        Actions.loginActions().doLogOut();
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
+
+        // Perform transaction
+        Actions.transactionActions().loginTeller();
+        Actions.transactionActions().goToTellerPage();
+        Actions.transactionActions().createGlDebitMiscCreditTransaction(glDebitMiscCreditTransaction);
+        Actions.transactionActions().clickCommitButton();
+        Pages.tellerPage().closeModal();
+
+        Actions.loginActions().doLogOutProgrammatically();
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
+
+        Actions.clientPageActions().searchAndOpenClientByName(chkAccountNumber);
+        AccountActions.editAccount().goToInstructionsTab();
+        String INSTRUCTION_REASON = "Reg CC";
+        AccountActions.createInstruction().deleteInstructionByReasonText(INSTRUCTION_REASON);
+
+        AccountActions.editAccount().goToDetailsTab();
+        Actions.clientPageActions().searchAndOpenClientByName(chkAccountNumber);
+        expectedBalanceData = AccountActions.retrievingAccountData().getBalanceDataForCHKAcc();
+        Actions.loginActions().doLogOut();
+    }
+
+    private void createDebitCard(String clientInitials, DebitCard debitCard) {
+        Actions.clientPageActions().searchAndOpenClientByName(clientInitials);
+        Pages.clientDetailsPage().clickOnMaintenanceTab();
+        Pages.clientDetailsPage().clickOnNewDebitCardButton();
+        Actions.debitCardModalWindowActions().fillDebitCard(debitCard);
+        Pages.debitCardModalWindow().clickOnSaveAndFinishButton();
+        Pages.debitCardModalWindow().waitForAddNewDebitCardModalWindowInvisibility();
+    }
+
+    private Map<String, String> getFieldsMap(NonTellerTransactionData transactionData) {
+        Map<String, String > result = new HashMap<>();
+        result.put("0", "0200");
+        result.put("3", "012000");
+        result.put("4", transactionData.getAmount());
+        result.put("11", String.valueOf(Generator.genInt(100000000, 922337203)));
+        result.put("18", "6011");
+        result.put("22", "021");
+        result.put("35", String.format("%s=%s", transactionData.getCardNumber(), transactionData.getExpirationDate()));
+        result.put("37", "201206102");
+        result.put("41", transactionData.getTerminalId());
+        result.put("43", "Long ave. bld. 34      Nashville      US");
+        result.put("48", "SHELL");
+        result.put("58", "00000000022");
+
+        return  result;
+    }
+}
