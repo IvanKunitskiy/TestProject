f040cb99286cfd4412496411a05d65c4493a0bcc
dbarsukov <dbarsukov@nymbus.com>
2020-10-05 18:31:38 +0300

    NBTDB-832: add waiter before setting cash drawer type, remove institution type in Trust account factory

--- src/main/java/com/nymbus/newmodels/generation/client/factory/basicinformation/type/organisation/TrustAccountFactory.java
+++ src/main/java/com/nymbus/newmodels/generation/client/factory/basicinformation/type/organisation/TrustAccountFactory.java
@@ -15,20 +15,11 @@ public class TrustAccountFactory implements OrganisationTypeFactory {
     @Override
     public OrganisationType getOrganisationType() {
         TrustAccount trustAccount = new TrustAccount();
-        setTrustAccountStatus(trustAccount);
+        trustAccount.setClientStatus(ClientStatus.MEMBER);
         trustAccount.setName(Random.genString(10));
         trustAccount.setTaxPayerIDType(TaxPayerIDType.BUSINESS_TIN);
         trustAccount.setTaxID(String.valueOf(new Timestamp(System.currentTimeMillis()).getTime()).substring(4));
 
         return trustAccount;
-    }
-
-    private void setTrustAccountStatus(TrustAccount client) {
-        if (Constants.INSTITUTION_TYPE.equals(FinancialInstitutionType.BANK)) {
-            client.setClientStatus(ClientStatus.CUSTOMER);
-        }
-        else {
-            client.setClientStatus(ClientStatus.MEMBER);
-        }
     }
 }
