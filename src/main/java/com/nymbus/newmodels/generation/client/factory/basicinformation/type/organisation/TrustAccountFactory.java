008a344b7a659cea3e3bcb076db9ede9dc6ddb46
dbarsukov <dbarsukov@nymbus.com>
2020-10-05 15:02:38 +0300

    NBTDB-832: add institution type for trust account builder

--- src/main/java/com/nymbus/newmodels/generation/client/factory/basicinformation/type/organisation/TrustAccountFactory.java
+++ src/main/java/com/nymbus/newmodels/generation/client/factory/basicinformation/type/organisation/TrustAccountFactory.java
@@ -1,6 +1,9 @@
 package com.nymbus.newmodels.generation.client.factory.basicinformation.type.organisation;
 
+import com.nymbus.core.utils.Constants;
+import com.nymbus.core.utils.FinancialInstitutionType;
 import com.nymbus.newmodels.client.basicinformation.type.TaxPayerIDType;
+import com.nymbus.newmodels.client.basicinformation.type.individual.Individual;
 import com.nymbus.newmodels.client.basicinformation.type.organisation.OrganisationType;
 import com.nymbus.newmodels.client.basicinformation.type.organisation.TrustAccount;
 import com.nymbus.newmodels.client.clientdetails.type.ClientStatus;
@@ -12,11 +15,20 @@ public class TrustAccountFactory implements OrganisationTypeFactory {
     @Override
     public OrganisationType getOrganisationType() {
         TrustAccount trustAccount = new TrustAccount();
-        trustAccount.setClientStatus(ClientStatus.MEMBER);
+        setTrustAccountStatus(trustAccount);
         trustAccount.setName(Random.genString(10));
         trustAccount.setTaxPayerIDType(TaxPayerIDType.BUSINESS_TIN);
         trustAccount.setTaxID(String.valueOf(new Timestamp(System.currentTimeMillis()).getTime()).substring(4));
 
         return trustAccount;
+    }
+
+    private void setTrustAccountStatus(TrustAccount client) {
+        if (Constants.INSTITUTION_TYPE.equals(FinancialInstitutionType.BANK)) {
+            client.setClientStatus(ClientStatus.CUSTOMER);
+        }
+        else {
+            client.setClientStatus(ClientStatus.MEMBER);
+        }
     }
 }
