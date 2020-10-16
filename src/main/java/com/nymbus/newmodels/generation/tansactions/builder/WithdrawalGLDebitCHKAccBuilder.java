106a7b80337d2e614520b76ef8cb77a7c03e61b9
dbarsukov <dbarsukov@nymbus.com>
2020-10-16 18:07:38 +0300

    NBTDB-832: add WithdrawalGLDebitCHKAccBuilder

---
+++ src/main/java/com/nymbus/newmodels/generation/tansactions/builder/WithdrawalGLDebitCHKAccBuilder.java
@@ -0,0 +1,45 @@
+package com.nymbus.newmodels.generation.tansactions.builder;
+
+import com.nymbus.actions.webadmin.WebAdminActions;
+import com.nymbus.newmodels.transaction.Transaction;
+import com.nymbus.newmodels.transaction.TransactionDestination;
+import com.nymbus.newmodels.transaction.TransactionSource;
+import com.nymbus.newmodels.transaction.enums.DestinationType;
+import com.nymbus.newmodels.transaction.enums.SourceType;
+import com.nymbus.util.Random;
+
+public class WithdrawalGLDebitCHKAccBuilder implements TransactionBuilder {
+    private Transaction transaction;
+
+    public WithdrawalGLDebitCHKAccBuilder() {
+        this.transaction =  new Transaction();
+    }
+
+    @Override
+    public void setDate() {
+        transaction.setTransactionDate(WebAdminActions.loginActions().getSystemDate());
+    }
+
+    @Override
+    public void setSource() {
+        transaction.setTransactionSource(new TransactionSource());
+        transaction.getTransactionSource().setSourceType(SourceType.WITHDRAWL);
+        transaction.getTransactionSource().setAccountNumber("83460855747");
+        transaction.getTransactionSource().setAmount(100.00);
+    }
+
+    @Override
+    public void setDestination() {
+        transaction.setTransactionDestination(new TransactionDestination());
+        transaction.getTransactionDestination().setSourceType(DestinationType.DEPOSIT);
+        transaction.getTransactionDestination().setAccountNumber("0-0-Dummy");
+        transaction.getTransactionDestination().setAmount(100.00);
+        transaction.getTransactionDestination().setTransactionCode("109 - Deposit");
+        transaction.getTransactionDestination().setNotes(Random.genString(20));
+    }
+
+    @Override
+    public Transaction getResult() {
+        return transaction;
+    }
+}
