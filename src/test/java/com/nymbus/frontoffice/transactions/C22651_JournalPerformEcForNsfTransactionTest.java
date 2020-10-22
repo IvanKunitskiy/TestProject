3e66c8cdb08a39304a926c978a38009a6579f9ab
pkyriushkin <pkyriushkin@nymbus.com>
2020-10-22 12:18:57 +0300

    NBTDB-832: Added a test class with precondition and test methods.

---
+++ src/test/java/com/nymbus/frontoffice/transactions/C22651_JournalPerformEcForNsfTransactionTest.java
@@ -0,0 +1,23 @@
+package com.nymbus.frontoffice.transactions;
+
+import com.nymbus.core.base.BaseTest;
+import io.qameta.allure.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("Transactions")
+@Owner("Petro")
+public class C22651_JournalPerformEcForNsfTransactionTest extends BaseTest {
+
+    @BeforeMethod
+    public void preCondition() {
+
+    }
+
+    @Test(description = "C22651, Journal: Perform EC for NSF transaction")
+    @Severity(SeverityLevel.CRITICAL)
+    public void journalPerformEcForNsfTransaction() {
+
+    }
+}
