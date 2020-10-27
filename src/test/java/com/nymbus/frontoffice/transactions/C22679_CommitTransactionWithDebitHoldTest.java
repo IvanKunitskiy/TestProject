4d770e71a45378cad45855f0b0f4c04cc64c543c
pkyriushkin <pkyriushkin@nymbus.com>
2020-10-27 11:30:50 +0200

    NBTDB-832: Added C22679 test class

---
+++ src/test/java/com/nymbus/frontoffice/transactions/C22679_CommitTransactionWithDebitHoldTest.java
@@ -0,0 +1,24 @@
+package com.nymbus.frontoffice.transactions;
+
+import com.nymbus.core.base.BaseTest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("Transactions")
+@Owner("Petro")
+public class C22679_CommitTransactionWithDebitHoldTest extends BaseTest {
+
+    @BeforeMethod
+    public void preCondition() {
+
+    }
+
+    @Test(description = "C22679, Commit transaction with debit hold")
+    public void commitTransactionWithDebitHold() {
+
+    }
+}
