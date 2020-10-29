50f95d9e4805c271ee62af05d51ba1cb663a97cf
pkyriushkin <pkyriushkin@nymbus.com>
2020-10-29 16:16:37 +0200

    NBTDB-832: Implemented C22680 TC.

---
+++ src/main/java/com/nymbus/newmodels/accountinstructions/CreditHoldInstruction.java
@@ -0,0 +1,22 @@
+package com.nymbus.newmodels.accountinstructions;
+
+import com.nymbus.core.utils.DateTime;
+import com.nymbus.util.Random;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class CreditHoldInstruction extends BaseInstruction {
+    private String expirationDate;
+    private String notes;
+
+    public String getType() {
+        return instructionType.getInstructionType();
+    }
+
+    public void updateInstructionData() {
+        this.expirationDate = DateTime.getDateTodayPlusDaysWithFormat(Random.genInt(10, 100), "MM/dd/yyyy");
+        this.notes = Random.genString(10);
+    }
+}
