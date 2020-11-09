6f1bf8b53247fb75e350f7212ffc8938ac2a4839
pkyriushkin <pkyriushkin@nymbus.com>
2020-11-09 15:49:03 +0200

    NBTDB-832: Added C25238 test class

---
+++ src/test/java/com/nymbus/frontoffice/depositaccountsmanagement/C25238_PrintTellerReceiptWithBalanceForDebitItemsTest.java
@@ -0,0 +1,14 @@
+package com.nymbus.frontoffice.depositaccountsmanagement;
+
+import com.nymbus.core.base.BaseTest;
+
+public class C25238_PrintTellerReceiptWithBalanceForDebitItemsTest extends BaseTest {
+
+    public void preCondition() {
+
+    }
+
+    public void printTellerReceiptWithBalanceForDebitItems() {
+
+    }
+}
