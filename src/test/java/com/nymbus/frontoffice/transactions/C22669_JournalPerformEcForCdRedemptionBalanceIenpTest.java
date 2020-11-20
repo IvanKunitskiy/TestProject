9b426738adb73aeff1af8e5e3969b6e113323344
pkyriushkin <pkyriushkin@nymbus.com>
2020-11-20 13:51:37 +0200

    NBTDB-832: added C22669_JournalPerformEcForCdRedemptionBalanceIenpTest test class

---
+++ src/test/java/com/nymbus/frontoffice/transactions/C22669_JournalPerformEcForCdRedemptionBalanceIenpTest.java
@@ -0,0 +1,24 @@
+package com.nymbus.frontoffice.transactions;
+
+import com.nymbus.core.base.BaseTest;
+import io.qameta.allure.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("Transactions")
+@Owner("Petro")
+public class C22669_JournalPerformEcForCdRedemptionBalanceIenpTest extends BaseTest {
+
+    @BeforeMethod
+    public void preCondition() {
+
+    }
+
+    @Test(description = "C22669, Journal: Perform EC for CD redemption (balance + IENP)")
+    @Severity(SeverityLevel.CRITICAL)
+    public void journalPerformEcForCdRedemptionBalanceIenp() {
+
+    }
+
+}
