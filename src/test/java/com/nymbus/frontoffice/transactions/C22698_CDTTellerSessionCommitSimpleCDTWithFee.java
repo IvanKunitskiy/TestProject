2abc4807255181c649950286c58bdb10c676679f
dbarsukov <dbarsukov@nymbus.com>
2020-11-24 17:12:14 +0200

    NBTDB-832: end test C22698

---
+++ src/test/java/com/nymbus/frontoffice/transactions/C22698_CDTTellerSessionCommitSimpleCDTWithFee.java
@@ -0,0 +1,171 @@
+package com.nymbus.frontoffice.transactions;
+
+import com.nymbus.actions.Actions;
+import com.nymbus.actions.account.AccountActions;
+import com.nymbus.actions.client.ClientsActions;
+import com.nymbus.core.base.BaseTest;
+import com.nymbus.core.utils.DateTime;
+import com.nymbus.newmodels.account.Account;
+import com.nymbus.newmodels.account.product.AccountType;
+import com.nymbus.newmodels.account.product.Products;
+import com.nymbus.newmodels.account.product.RateType;
+import com.nymbus.newmodels.client.IndividualClient;
+import com.nymbus.newmodels.generation.client.builder.IndividualClientBuilder;
+import com.nymbus.newmodels.generation.client.builder.type.individual.IndividualBuilder;
+import com.nymbus.newmodels.generation.tansactions.TransactionConstructor;
+import com.nymbus.newmodels.generation.tansactions.builder.GLDebitDepositCHKAccBuilder;
+import com.nymbus.newmodels.generation.tansactions.builder.WithdrawalGLDebitCHKAccBuilder;
+import com.nymbus.newmodels.transaction.Transaction;
+import com.nymbus.newmodels.transaction.verifyingModels.BalanceDataForCHKAcc;
+import com.nymbus.newmodels.transaction.verifyingModels.TransactionData;
+import com.nymbus.pages.Pages;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class C22698_CDTTellerSessionCommitSimpleCDTWithFee extends BaseTest {
+    private Transaction transaction;
+    private Transaction savingsTransaction;
+    private BalanceDataForCHKAcc expectedBalanceData;
+    private BalanceDataForCHKAcc expectedSavingsBalanceData;
+    private TransactionData chkAccTransactionData;
+    private TransactionData savingsAccTransactionData;
+    private Account checkAccount;
+    private Account savingsAccount;
+    private double transactionAmount = 200.00;
+    private double savingsTransactionAmount = 200.00;
+    private double returnTransactionAmount = 100.00;
+    private double fee = 3.00;
+
+
+    @BeforeMethod
+    public void prepareTransactionData() {
+        // Set up Client and Accounts
+        IndividualClientBuilder individualClientBuilder = new IndividualClientBuilder();
+        individualClientBuilder.setIndividualClientBuilder(new IndividualBuilder());
+        IndividualClient client = individualClientBuilder.buildClient();
+        checkAccount = new Account().setCHKAccountData();
+        savingsAccount = new Account().setSavingsAccountData();
+        Transaction depositTransaction = new TransactionConstructor(new GLDebitDepositCHKAccBuilder()).constructTransaction();
+        Transaction depositSavingsTransaction = new TransactionConstructor(new GLDebitDepositCHKAccBuilder()).constructTransaction();
+        transaction = new TransactionConstructor(new WithdrawalGLDebitCHKAccBuilder()).constructTransaction();
+        savingsTransaction = new TransactionConstructor(new WithdrawalGLDebitCHKAccBuilder()).constructTransaction();
+
+        // Log in
+        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+
+        // Set products
+        checkAccount.setProduct(Actions.productsActions().getProduct(Products.CHK_PRODUCTS, AccountType.CHK, RateType.FIXED));
+        savingsAccount.setProduct(Actions.productsActions().getProduct(Products.SAVINGS_PRODUCTS, AccountType.REGULAR_SAVINGS, RateType.FIXED));
+
+        // Create client
+        ClientsActions.individualClientActions().createClient(client);
+        ClientsActions.individualClientActions().setClientDetailsData(client);
+        ClientsActions.individualClientActions().setDocumentation(client);
+
+
+        // Create account
+        AccountActions.createAccount().createCHKAccountForTransactionPurpose(checkAccount);
+        Pages.accountDetailsPage().clickAccountsLink();
+        AccountActions.createAccount().createSavingsAccount(savingsAccount);
+
+        // Set up transactions with account number
+        depositTransaction.getTransactionDestination().setAccountNumber(checkAccount.getAccountNumber());
+        depositTransaction.getTransactionDestination().setAmount(transactionAmount);
+        depositTransaction.getTransactionSource().setAmount(transactionAmount);
+        transaction.getTransactionSource().setAccountNumber(checkAccount.getAccountNumber());
+        transaction.getTransactionSource().setAmount(returnTransactionAmount);
+        transaction.getTransactionDestination().setAmount(returnTransactionAmount);
+        depositSavingsTransaction.getTransactionDestination().setAccountNumber(savingsAccount.getAccountNumber());
+        depositSavingsTransaction.getTransactionDestination().setTransactionCode("209 - Deposit");
+        depositSavingsTransaction.getTransactionDestination().setAmount(savingsTransactionAmount);
+        depositSavingsTransaction.getTransactionSource().setAmount(savingsTransactionAmount);
+
+        // Perform deposit transactions
+        Actions.transactionActions().goToTellerPage();
+        Actions.transactionActions().doLoginTeller();
+        Actions.transactionActions().createTransaction(depositTransaction);
+        Actions.transactionActions().clickCommitButton();
+        Pages.tellerPage().closeModal();
+        Actions.transactionActions().goToTellerPage();
+        Actions.transactionActions().createTransaction(depositSavingsTransaction);
+        Actions.transactionActions().clickCommitButton();
+        Pages.tellerPage().closeModal();
+
+        Actions.loginActions().doLogOutProgrammatically();
+        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+
+        // Set transaction with amount value
+        Actions.clientPageActions().searchAndOpenClientByName(checkAccount.getAccountNumber());
+        expectedBalanceData = AccountActions.retrievingAccountData().getBalanceDataForCHKAcc();
+        chkAccTransactionData = new TransactionData(DateTime.getLocalDateOfPattern("MM/dd/yyyy"), DateTime.getLocalDateOfPattern("MM/dd/yyyy"),
+                "-", expectedBalanceData.getCurrentBalance(), returnTransactionAmount);
+
+        Actions.clientPageActions().searchAndOpenClientByName(savingsAccount.getAccountNumber());
+        expectedSavingsBalanceData = AccountActions.retrievingAccountData().getBalanceDataForCHKAcc();
+        savingsAccTransactionData = new TransactionData(DateTime.getLocalDateOfPattern("MM/dd/yyyy"), DateTime.getLocalDateOfPattern("MM/dd/yyyy"),
+                "-", expectedSavingsBalanceData.getCurrentBalance(), savingsTransactionAmount);
+        Actions.loginActions().doLogOut();
+    }
+
+    @Test(description = "C22697, CDT+Teller Session - Commit simple CDT transfer with no fee")
+    @Severity(SeverityLevel.CRITICAL)
+    public void printTellerReceiptWithoutBalance() {
+        logInfo("Step 1: Log in to the system as User from the preconditions");
+        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+
+        logInfo("Step 2: Go to Cashier Defined Transactions screen and log in to proof date");
+        Actions.transactionActions().loginTeller();
+        Pages.aSideMenuPage().waitForASideMenu();
+        Pages.aSideMenuPage().clickCashierDefinedTransactionsMenuItem();
+
+        logInfo("Step 3: Search for template from preconditions and select it");
+        logInfo("Step 4: Specify accounts from preconditions in source and destination line items;\n" +
+                "set transaction amount less than Debit Account's Available Balance\n" +
+                "Enter any value to the 'Notes' field;");
+        Actions.cashierDefinedActions().createTransaction("TRANSFER FROM SAV TO CHK", transaction);
+        expectedBalanceData.reduceAmount(transaction.getTransactionDestination().getAmount() + fee);
+        expectedSavingsBalanceData.addAmount(transaction.getTransactionDestination().getAmount());
+
+        logInfo("Step 5: Click [Commit Transaction] button");
+        Actions.transactionActions().clickCommitButton();
+
+        logInfo("Step 6: Go to account used in DEBIT item and verify its:\n" +
+                "- current balance\n" +
+                "- available balance");
+        Actions.clientPageActions().searchAndOpenClientByName(checkAccount.getAccountNumber());
+        BalanceDataForCHKAcc actualBalanceData = AccountActions.retrievingAccountData().getBalanceDataForCHKAcc();
+
+        Assert.assertEquals(actualBalanceData.getCurrentBalance(), expectedBalanceData.getCurrentBalance(),
+                "Current balance doesn't match!");
+        Assert.assertEquals(actualBalanceData.getAvailableBalance(), expectedBalanceData.getAvailableBalance(),
+                "Available balance doesn't match!");
+
+        logInfo("Step 7: Open account on the Transactions tab and verify the committed transaction");
+        Pages.accountDetailsPage().clickTransactionsTab();
+        chkAccTransactionData.setBalance(expectedBalanceData.getCurrentBalance());
+        AccountActions.retrievingAccountData().goToTransactionsTab();
+        TransactionData actualTransactionData = AccountActions.retrievingAccountData().getTransactionDataWithBalanceSymbol();
+        Assert.assertEquals(actualTransactionData, chkAccTransactionData, "Transaction data doesn't match!");
+
+        logInfo("Step 8: Go to account used in CREDIT item and verify its:\n" +
+                "- current balance\n" +
+                "- available balance");
+        Actions.clientPageActions().searchAndOpenClientByName(savingsAccount.getAccountNumber());
+        BalanceDataForCHKAcc actualSavBalanceData = AccountActions.retrievingAccountData().getBalanceDataForCHKAcc();
+
+        Assert.assertEquals(actualSavBalanceData.getCurrentBalance(), expectedSavingsBalanceData.getCurrentBalance(),
+                "Current balance doesn't match!");
+        Assert.assertEquals(actualSavBalanceData.getAvailableBalance(), expectedSavingsBalanceData.getAvailableBalance(),
+                "Available balance doesn't match!");
+
+        logInfo("Step 9: Open account on the Transactions tab and verify the committed transaction");
+        Pages.accountDetailsPage().clickTransactionsTab();
+        savingsAccTransactionData.setBalance(expectedSavingsBalanceData.getCurrentBalance());
+        AccountActions.retrievingAccountData().goToTransactionsTab();
+        TransactionData actualSavTransactionData = AccountActions.retrievingAccountData().getTransactionDataWithBalanceSymbol();
+        Assert.assertEquals(actualSavTransactionData, savingsAccTransactionData, "Transaction data doesn't match!");
+    }
+}
