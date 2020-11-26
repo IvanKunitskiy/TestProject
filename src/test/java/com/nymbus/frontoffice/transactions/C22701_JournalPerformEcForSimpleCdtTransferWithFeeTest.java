41b9befbd0ddcdb0d0b252e9421cda1fe9c89a9c
pkyriushkin <pkyriushkin@nymbus.com>
2020-11-26 13:54:36 +0200

    NBTDB-832: C22701 - initial commit. added test class.

---
+++ src/test/java/com/nymbus/frontoffice/transactions/C22701_JournalPerformEcForSimpleCdtTransferWithFeeTest.java
@@ -0,0 +1,23 @@
+package com.nymbus.frontoffice.transactions;
+
+import com.nymbus.core.base.BaseTest;
+import io.qameta.allure.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("Transactions")
+@Owner("Petro")
+public class C22701_JournalPerformEcForSimpleCdtTransferWithFeeTest extends BaseTest {
+
+    @BeforeMethod
+    public void preCondition() {
+
+    }
+
+    @Test(description = "C22701, Journal - Perform EC for simple CDT transfer with fee")
+    @Severity(SeverityLevel.CRITICAL)
+    public void journalPerformEcForSimpleCdtTransferWithFeeTest() {
+
+    }
+}
