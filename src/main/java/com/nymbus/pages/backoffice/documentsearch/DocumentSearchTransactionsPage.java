aa5d8d65fea6ddbf7874a05ff01e7120f167b6d5
pkyriushkin <pkyriushkin@nymbus.com>
2020-12-21 18:52:33 +0200

    NBTDB-832: Paused. Awaiting for a feedback on assertion in step 4

--- src/main/java/com/nymbus/pages/backoffice/documentsearch/DocumentSearchTransactionsPage.java
+++ src/main/java/com/nymbus/pages/backoffice/documentsearch/DocumentSearchTransactionsPage.java
@@ -12,13 +12,16 @@ public class DocumentSearchTransactionsPage extends PageTools {
     private final By transactionsTab = By.xpath("//ul/li/a[text()='Transactions']");
     private final By searchButton = By.xpath("//button[text()='Search']");
     private final By tabContent = By.xpath("//article[@ui-view='tabContent']");
-    private final By tableLineByIndex = By.xpath("//div[@class='gridok_content']/div/div[contains(@class, 'dn-gridok-table__tbody')]" +
+    private final By lineCheckboxByIndex = By.xpath("//div[@class='gridok_content']/div/div[contains(@class, 'dn-gridok-table__tbody')]" +
             "/div[%s]/div[contains(@class, 'checkbox-column')]//input");
     private final By showCompanionItemsButton = By.xpath("//button[div/span[text()='Show Companion Items']]");
     private final By companionItemsPanel = By.xpath("//div[contains(@class, 'showOnCompanionItems')]");
     private final By companionItem = By.xpath("//div[contains(@class, 'showOnCompanionItems')]/div/div[contains(@class, 'dn-gridok-table__tbody')]");
     private final By clearAllButton = By.xpath("//button[text()='Clear all']");
     private final By transactionItem = By.xpath("//div[contains(@class, 'dn-gridok-table__tbody')]//div[contains(@class, 'dn-gridok-table__tr')]");
+    private final By dateInput = By.xpath("//input[@id='proofdatefrom']");
+    private final By checkNumberInput = By.xpath("//input[@name='checknumberfrom']");
+    private final By tableLineByIndex = By.xpath("//div[@class='gridok_content']/div/div[contains(@class, 'dn-gridok-table__tbody')]/div[%s]");
 
     @Step("Click 'Transactions' tab")
     public void clickTransactionsTab() {
@@ -36,6 +39,18 @@ public class DocumentSearchTransactionsPage extends PageTools {
     public void setAmount(String amount) {
         waitForElementVisibility(amountInput);
         type(amount, amountInput);
+    }
+
+    @Step("Set 'Date' value")
+    public void setDate(String date) {
+        waitForElementVisibility(dateInput);
+        type(date, dateInput);
+    }
+
+    @Step("Set 'Check #' value")
+    public void setCheckNumber(String checkNumber) {
+        waitForElementVisibility(checkNumberInput);
+        type(checkNumber, checkNumberInput);
     }
 
     @Step("Click 'Search' button")
@@ -56,6 +71,11 @@ public class DocumentSearchTransactionsPage extends PageTools {
 
     @Step("Select transaction line by index")
     public void selectLineByIndex(int index) {
+        click(lineCheckboxByIndex, index);
+    }
+
+    @Step("Click transaction line by index")
+    public void clickLineByIndex(int index) {
         click(tableLineByIndex, index);
     }
 
---
+++ src/test/java/com/nymbus/frontoffice/backoffice/C22847_DocumentSearchSearchTransactionsUsingFiltersTest.java
@@ -0,0 +1,82 @@
+package com.nymbus.frontoffice.backoffice;
+
+import com.nymbus.actions.Actions;
+import com.nymbus.actions.webadmin.WebAdminActions;
+import com.nymbus.core.base.BaseTest;
+import com.nymbus.core.utils.Constants;
+import com.nymbus.core.utils.DateTime;
+import com.nymbus.core.utils.SelenideTools;
+import com.nymbus.newmodels.transaction.verifyingModels.WebAdminTransactionFromQuery;
+import com.nymbus.pages.Pages;
+import io.qameta.allure.*;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Backoffice")
+@Feature("Notices")
+@Owner("Petro")
+public class C22847_DocumentSearchSearchTransactionsUsingFiltersTest extends BaseTest {
+
+    private WebAdminTransactionFromQuery T1;
+    private WebAdminTransactionFromQuery T2;
+    private WebAdminTransactionFromQuery T3;
+
+    @BeforeMethod
+    public void preCondition() {
+        SelenideTools.openUrl(Constants.WEB_ADMIN_URL);
+        WebAdminActions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+
+        T1 = WebAdminActions.webAdminUsersActions().getCheckTransaction();
+        T2 = WebAdminActions.webAdminUsersActions().getAtmTransaction();
+        T3 = WebAdminActions.webAdminUsersActions().getTransactionCommittedOnCurrentDate();
+
+        WebAdminActions.loginActions().doLogout();
+    }
+
+    @Test(description="C22847, Document Search: Search transactions using filters")
+    @Severity(SeverityLevel.CRITICAL)
+    public void documentSearchSearchTransactionsUsingFilters() {
+
+        logInfo("Step 1: Log in to the system as the user from the precondition");
+        SelenideTools.openUrl(Constants.URL);
+        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+
+        logInfo("Step 2: Go to Backoffice > Document Search > Transactions tab");
+        Pages.aSideMenuPage().clickBackOfficeMenuItem();
+        Pages.backOfficePage().clickDocumentSearchSeeMoreLink();
+        Pages.documentSearchTransactionsPage().waitForTabContent();
+
+        logInfo("Step 3: Specify date == date of T1 from preconditions (e.g. 12/12/2020),\n" +
+                "check number XXXX in filters section; Run the search");
+        Pages.documentSearchTransactionsPage().setDate(T1.getEffectiveEntryDate());
+        Pages.documentSearchTransactionsPage().setCheckNumber(T1.getCheckNumber());
+        Pages.documentSearchTransactionsPage().clickSearchButton();
+        Pages.documentSearchTransactionsPage().waitForSearchButtonClickable();
+        Assert.assertTrue(Pages.documentSearchTransactionsPage().getTransactionsCount() >= 1,
+                "There are no transactions found");
+
+        logInfo("Step 4: Click the found transaction");
+        Pages.documentSearchTransactionsPage().clickLineByIndex(1);
+        // TODO: verify check in images section
+
+        logInfo("Step 5: Specify amount == amount of T2 from preconditions (e.g. $200), \n" +
+                "account number related to T2 and branch2 in filters section; Run the search");
+        Pages.documentSearchTransactionsPage().clickClearAllButton();
+        Pages.documentSearchTransactionsPage().setAmount(String.valueOf(T2.getAmount()));
+        Pages.documentSearchTransactionsPage().setAccount(T2.getAccountNumber());
+        Pages.documentSearchTransactionsPage().setDate(T2.getEffectiveEntryDate());
+        Pages.documentSearchTransactionsPage().clickSearchButton();
+        Pages.documentSearchTransactionsPage().waitForSearchButtonClickable();
+        Assert.assertTrue(Pages.documentSearchTransactionsPage().getTransactionsCount() >= 1,
+                "There are no transactions found");
+
+        logInfo("Step 6: Specify current date in filters section; Run the search");
+        Pages.documentSearchTransactionsPage().clickClearAllButton();
+        Pages.documentSearchTransactionsPage().setDate(DateTime.getLocalDate());
+        Pages.documentSearchTransactionsPage().clickSearchButton();
+        Pages.documentSearchTransactionsPage().waitForSearchButtonClickable();
+        Assert.assertTrue(Pages.documentSearchTransactionsPage().getTransactionsCount() >= 1,
+                "There are no transactions found");
+    }
+}
