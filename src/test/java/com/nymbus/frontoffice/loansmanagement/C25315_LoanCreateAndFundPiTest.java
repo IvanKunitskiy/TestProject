ab3153698e5de828fca15f6efd52db0292d932d0
pkyriushkin <pkyriushkin@nymbus.com>
2021-01-05 11:36:02 +0200

    NBTDB-832: Added test class for C25315 TC

---
+++ src/test/java/com/nymbus/frontoffice/loansmanagement/C25315_LoanCreateAndFundPiTest.java
@@ -0,0 +1,24 @@
+package com.nymbus.frontoffice.loansmanagement;
+
+import com.nymbus.core.base.BaseTest;
+import io.qameta.allure.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("Loans Management")
+@Owner("Petro")
+public class C25315_LoanCreateAndFundPiTest extends BaseTest {
+
+    @BeforeMethod
+    public void preCondition() {
+
+    }
+
+    @Test(description = "C25315, Loan - Create and Fund: P&I (bill)")
+    @Severity(SeverityLevel.CRITICAL)
+    public void loanCreateAndFundPi() {
+
+    }
+
+}
