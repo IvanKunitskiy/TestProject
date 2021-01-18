017a89f9cdf63a89b8c21bf852e7cda7e219ae5d
pkyriushkin <pkyriushkin@nymbus.com>
2021-01-18 12:21:15 +0200

    NBTDB-832: C25413 - initial commit.

---
+++ src/test/java/com/nymbus/frontoffice/loansmanagement/C25413_ChangeMaturityDateForLoanAccountTest.java
@@ -0,0 +1,23 @@
+package com.nymbus.frontoffice.loansmanagement;
+
+import com.nymbus.core.base.BaseTest;
+import io.qameta.allure.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("Loans Management")
+@Owner("Petro")
+public class C25413_ChangeMaturityDateForLoanAccountTest extends BaseTest {
+
+    @BeforeMethod
+    public void preCondition() {
+
+    }
+
+    @Test(description = "C25413, Change Maturity Date for loan account")
+    @Severity(SeverityLevel.CRITICAL)
+    public void changeMaturityDateForLoanAccount() {
+
+    }
+}
