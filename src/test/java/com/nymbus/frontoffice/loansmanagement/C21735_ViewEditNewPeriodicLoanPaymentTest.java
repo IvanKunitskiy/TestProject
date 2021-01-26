2db24b1502e11823125f3b097af4a74be529dd0f
pkyriushkin <pkyriushkin@nymbus.com>
2021-01-26 13:50:08 +0200

    NBTDB-832: C21735 initial commit.

---
+++ src/test/java/com/nymbus/frontoffice/loansmanagement/C21735_ViewEditNewPeriodicLoanPaymentTest.java
@@ -0,0 +1,25 @@
+package com.nymbus.frontoffice.loansmanagement;
+
+import com.nymbus.core.base.BaseTest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("Loans Management")
+@Owner("Petro")
+public class C21735_ViewEditNewPeriodicLoanPaymentTest extends BaseTest {
+
+    @BeforeMethod
+    public void preCondition() {
+
+    }
+
+    @Test(description = "C21735, View / edit new periodic loan payment")
+    public void viewEditNewPeriodicLoanPayment() {
+
+    }
+
+}
