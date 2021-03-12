669d840e377984e7345cb22b024b6fb6cc0bcadf
pkyriushkin <pkyriushkin@nymbus.com>
2021-03-12 11:47:01 +0200

    NBTDB-832: staged c25481.

---
+++ src/test/java/com/nymbus/frontoffice/loansmanagement/C25481_ProcessEc416LoanPrepaymentTransactionPrinIntBillTest.java
@@ -0,0 +1,211 @@
+package com.nymbus.frontoffice.loansmanagement;
+
+import com.nymbus.actions.Actions;
+import com.nymbus.actions.account.AccountActions;
+import com.nymbus.actions.client.ClientsActions;
+import com.nymbus.core.base.BaseTest;
+import com.nymbus.newmodels.account.Account;
+import com.nymbus.newmodels.account.loanaccount.PaymentAmountType;
+import com.nymbus.newmodels.account.product.AccountType;
+import com.nymbus.newmodels.account.product.Products;
+import com.nymbus.newmodels.account.product.RateType;
+import com.nymbus.newmodels.client.IndividualClient;
+import com.nymbus.newmodels.generation.client.builder.IndividualClientBuilder;
+import com.nymbus.newmodels.generation.client.builder.type.individual.IndividualBuilder;
+import com.nymbus.newmodels.generation.tansactions.TransactionConstructor;
+import com.nymbus.newmodels.generation.tansactions.builder.GLDebitDepositCHKAccBuilder;
+import com.nymbus.newmodels.generation.tansactions.builder.MiscDebitMiscCreditBuilder;
+import com.nymbus.newmodels.generation.tansactions.factory.DestinationFactory;
+import com.nymbus.newmodels.generation.tansactions.factory.SourceFactory;
+import com.nymbus.newmodels.transaction.Transaction;
+import com.nymbus.newmodels.transaction.TransactionDestination;
+import com.nymbus.newmodels.transaction.TransactionSource;
+import com.nymbus.newmodels.transaction.enums.TransactionCode;
+import com.nymbus.pages.Pages;
+import com.nymbus.testrail.CustomStepResult;
+import com.nymbus.testrail.TestRailAssert;
+import com.nymbus.testrail.TestRailIssue;
+import io.qameta.allure.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("Loans Management")
+@Owner("Petro")
+public class C25481_ProcessEc416LoanPrepaymentTransactionPrinIntBillTest extends BaseTest {
+
+    private Account loanAccount;
+    private Transaction transaction_416;
+    private final String loanProductName = "Test Loan Product";
+    private final String loanProductInitials = "TLP";
+    private final TransactionSource miscDebitSource = SourceFactory.getMiscDebitSource();
+    private final TransactionDestination miscCreditDestination = DestinationFactory.getMiscCreditDestination();
+    private final String TEST_RUN_NAME = "Loans Management";
+    private String nextPaymentBilledDueDate;
+    private double interest;
+    private int transaction109Amount;
+
+    @BeforeMethod
+    public void preCondition() {
+        // Set up Client
+        IndividualClientBuilder individualClientBuilder = new IndividualClientBuilder();
+        individualClientBuilder.setIndividualClientBuilder(new IndividualBuilder());
+        IndividualClient client = individualClientBuilder.buildClient();
+
+        // Set up accounts
+        Account chkAccount = new Account().setCHKAccountData();
+        loanAccount = new Account().setLoanAccountData();
+        loanAccount.setProduct(loanProductName);
+        loanAccount.setMailCode(client.getIndividualClientDetails().getMailCode().getMailCode());
+        loanAccount.setPaymentAmountType(PaymentAmountType.PRINCIPAL_AND_INTEREST.getPaymentAmountType());
+
+        // Login to the system
+        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+
+        // Check that a Loan product exist with the following editable fields (Readonly? = NO) and create if not exist
+        Actions.loanProductOverviewActions().checkLoanProductExistAndCreateIfFalse(loanProductName, loanProductInitials);
+        Actions.loginActions().doLogOut();
+
+        // Log in
+        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+
+        // Set products
+        chkAccount.setProduct(Actions.productsActions().getProduct(Products.CHK_PRODUCTS, AccountType.CHK, RateType.FIXED));
+
+        // Create client
+        ClientsActions.individualClientActions().createClient(client);
+        ClientsActions.individualClientActions().setClientDetailsData(client);
+        ClientsActions.individualClientActions().setDocumentation(client);
+
+        // Create account
+        AccountActions.createAccount().createCHKAccount(chkAccount);
+        Pages.accountNavigationPage().clickAccountsInBreadCrumbs();
+        AccountActions.createAccount().createLoanAccount(loanAccount);
+        String clientRootId = ClientsActions.createClient().getClientIdFromUrl();
+
+        // Set up deposit transaction
+        int depositTransactionAmount = 12000;
+        Transaction depositTransaction = new TransactionConstructor(new GLDebitDepositCHKAccBuilder()).constructTransaction();
+        depositTransaction.getTransactionDestination().setAccountNumber(chkAccount.getAccountNumber());
+        depositTransaction.getTransactionDestination().setAmount(depositTransactionAmount);
+        depositTransaction.getTransactionSource().setAmount(depositTransactionAmount);
+
+        // Set up 109 transaction
+        transaction109Amount = 12000;
+        miscDebitSource.setAccountNumber(loanAccount.getAccountNumber());
+        miscDebitSource.setTransactionCode(TransactionCode.NEW_LOAN_411.getTransCode());
+        miscDebitSource.setAmount(transaction109Amount);
+        miscCreditDestination.setAccountNumber(chkAccount.getAccountNumber());
+        miscCreditDestination.setTransactionCode(TransactionCode.ATM_DEPOSIT_109.getTransCode());
+        miscCreditDestination.setAmount(transaction109Amount);
+
+        // Set up 416 transaction
+        transaction_416 = new TransactionConstructor(new MiscDebitMiscCreditBuilder()).constructTransaction();
+        transaction_416.getTransactionSource().setTransactionCode(TransactionCode.LOAN_PAYMENT_114.getTransCode());
+        transaction_416.getTransactionSource().setAccountNumber(chkAccount.getAccountNumber());
+        transaction_416.getTransactionDestination().setTransactionCode(TransactionCode.PAYMENT_416.getTransCode());
+        transaction_416.getTransactionDestination().setAccountNumber(loanAccount.getAccountNumber());
+
+        // Perform deposit transaction
+        Actions.loginActions().doLogOutProgrammatically();
+        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+        Actions.transactionActions().goToTellerPage();
+        Actions.transactionActions().doLoginTeller();
+        Actions.transactionActions().createTransaction(depositTransaction);
+        Actions.transactionActions().clickCommitButton();
+        Pages.tellerPage().closeModal();
+
+        Actions.loginActions().doLogOutProgrammatically();
+        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+
+        // Perform 109 transaction
+        Actions.transactionActions().goToTellerPage();
+        Actions.transactionActions().doLoginTeller();
+        Actions.transactionActions().setMiscDebitSource(miscDebitSource, 0);
+        Actions.transactionActions().setMiscCreditDestination(miscCreditDestination, 0);
+        Pages.tellerPage().setEffectiveDate(loanAccount.getDateOpened());
+        Actions.transactionActions().clickCommitButtonWithProofDateModalVerification();
+        Pages.tellerPage().closeModal();
+
+        // Perform nonTellerTransactions
+        Actions.nonTellerTransaction().generatePaymentDueRecord(clientRootId);
+        Actions.loginActions().doLogOutProgrammatically();
+
+        // Retrieve amount due value
+        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+        Pages.aSideMenuPage().clickClientMenuItem();
+        Actions.clientPageActions().searchAndOpenAccountByAccountNumber(loanAccount.getAccountNumber());
+        nextPaymentBilledDueDate = Pages.accountDetailsPage().getNextPaymentBilledDueDate();
+        Pages.accountDetailsPage().clickPaymentInfoTab();
+        double amountDue = Double.parseDouble(Pages.accountPaymentInfoPage().getAmountDueFromRecordByIndex(1));
+        Pages.accountPaymentInfoPage().clickPaymentDueRecord();
+        transaction_416.getTransactionSource().setAmount(amountDue);
+        transaction_416.getTransactionDestination().setAmount(amountDue);
+
+        // Perform 416 transaction
+        Actions.transactionActions().goToTellerPage();
+        Actions.transactionActions().doLoginTeller();
+        Actions.transactionActions().setMiscDebitSourceForWithDraw(transaction_416.getTransactionSource(), 0);
+        Actions.transactionActions().setMiscCreditDestination(transaction_416.getTransactionDestination(), 0);
+        Actions.transactionActions().clickCommitButton();
+        Pages.tellerPage().closeModal();
+        Actions.loginActions().doLogOutProgrammatically();
+
+        // Retrieve interest value
+        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+        Pages.aSideMenuPage().clickClientMenuItem();
+        Actions.clientPageActions().searchAndOpenAccountByAccountNumber(loanAccount.getAccountNumber());
+        Pages.accountDetailsPage().clickPaymentInfoTab();
+        Pages.accountPaymentInfoPage().clickPaymentDueRecord();
+        interest = Double.parseDouble(Pages.accountPaymentInfoPage().getInterest());
+        Actions.loginActions().doLogOut();
+    }
+
+    @TestRailIssue(issueID = 25481, testRunName = TEST_RUN_NAME)
+    @Test(description = "C25481, Process EC 416 loan prepayment transaction. Prin&Int (bill)")
+    @Severity(SeverityLevel.CRITICAL)
+    public void processEc416LoanPrepaymentTransactionPrinIntBill() {
+
+        logInfo("Step 1: Log in to the system");
+        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+
+        logInfo("Step 2: Go to 'Journal' page in the 'Menu' on the left side of the screen");
+        Actions.transactionActions().loginTeller();
+        Actions.journalActions().goToJournalPage();
+
+        logInfo("Step 3: Find and select a transaction from precondition 4");
+        Actions.journalActions().applyFilterByAccountNumber(transaction_416.getTransactionDestination().getAccountNumber());
+        Actions.journalActions().clickLastTransaction();
+
+        logInfo("Step 4: Click on the 'Error Correct' button");
+        Pages.journalDetailsPage().clickErrorCorrectButton();
+        Pages.journalDetailsPage().waitForErrorCorrectButtonInvisibility();
+        TestRailAssert.assertTrue(Actions.journalActions().getTransactionState().equals("Void"),
+                new CustomStepResult("Transaction state hasn't changed", "Transaction state has changed"));
+
+        logInfo("Step 5: Open loan account from preconditions on the 'Details' tab");
+        Pages.aSideMenuPage().clickClientMenuItem();
+        Actions.clientPageActions().searchAndOpenAccountByAccountNumber(loanAccount);
+
+        logInfo("Step 6: Pay attention to the following fields:\n" +
+                "- Next Payment Billed Due Date\n" +
+                "- Interest paid to date\n" +
+                "- Date Last Payment\n" +
+                "- Date interest paid thru\n" +
+                "- Current Balance\n" +
+                "- Accrued Interest\n");
+        TestRailAssert.assertTrue(Pages.accountDetailsPage().getNextPaymentBilledDueDate().equals(nextPaymentBilledDueDate),
+                new CustomStepResult("'Next Payment Billed Due Date' is not valid", "'Next Payment Billed Due Date' is valid"));
+        TestRailAssert.assertTrue(Pages.accountDetailsPage().getInterestPaidToDate().equals("0.00"),
+                new CustomStepResult("'Interest Paid To Date' is not valid", "'Interest Paid To Date' is valid"));
+        TestRailAssert.assertTrue(Pages.accountDetailsPage().getDateInterestPaidThru().isEmpty(),
+                new CustomStepResult("'Date interest paid thru' is not valid", "'Date interest paid thru' is valid"));
+        TestRailAssert.assertTrue(Pages.accountDetailsPage().getDateLastPayment().isEmpty(),
+                new CustomStepResult("'Date last payment' is not valid", "'Date last payment' is valid"));
+        TestRailAssert.assertTrue(Double.parseDouble(Pages.accountDetailsPage().getCurrentBalance()) == transaction109Amount,
+                new CustomStepResult("'Current Balance' is not valid", "'Current Balance' is valid"));
+        double actualAccruedInterest = Double.parseDouble(Pages.accountDetailsPage().getAccruedInterest());
+        TestRailAssert.assertTrue(actualAccruedInterest == interest,
+                new CustomStepResult("'Accrued Interest' is not valid", "'Accrued Interest' is valid"));
+    }
+}
