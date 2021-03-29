080149cc525eadf271928e976f9eb9476dcf8db5
dbarsukov <dbarsukov@nymbus.com>
2021-03-29 17:41:08 +0300

    NBTDB-832: remove unused transactions

--- src/test/java/com/nymbus/frontoffice/loansmanagement/C25376_PaymentDueDateEditPartialPaymentForPrincipalAndInterestPaymentDueRecordTest.java
+++
@@ -1,6 +0,0 @@
-package com.nymbus.frontoffice.loansmanagement;
-
-import com.nymbus.core.base.BaseTest;
-
-public class C25376_PaymentDueDateEditPartialPaymentForPrincipalAndInterestPaymentDueRecordTest extends BaseTest {
-}
