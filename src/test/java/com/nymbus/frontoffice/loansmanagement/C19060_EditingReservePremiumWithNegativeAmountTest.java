d7581124df51de128e07be636b2adc724152e5da
pkyriushkin <pkyriushkin@nymbus.com>
2021-04-28 11:08:59 +0300

    NBTDB-832: fixed 'clickViewAllLoanReserveLink' method usage

--- src/test/java/com/nymbus/frontoffice/loansmanagement/C19060_EditingReservePremiumWithNegativeAmountTest.java
+++ src/test/java/com/nymbus/frontoffice/loansmanagement/C19060_EditingReservePremiumWithNegativeAmountTest.java
@@ -101,7 +101,7 @@ public class C19060_EditingReservePremiumWithNegativeAmountTest extends BaseTest
 
         Pages.aSideMenuPage().clickLoansMenuItem();
         Pages.loansPage().waitForLoanPageLoaded();
-        Pages.loansPage().clickViewAllLoanReserveLink();
+        Pages.loansPage().clickViewAllLoanReserves();
         Pages.loanReservePage().clickAddNewButton();
         Pages.addNewLoanReservePage().typeReservePremiumAmortizationCodeField(loanReserve.getReservePremiumAmortizationCode());
         Pages.addNewLoanReservePage().clickBalanceDefinitionSelectorButton();
