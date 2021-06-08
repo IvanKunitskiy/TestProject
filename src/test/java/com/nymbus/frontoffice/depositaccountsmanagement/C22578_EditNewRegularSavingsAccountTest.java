d1fbe2d8c5f50ea3b6cbc608f620b99971ae1fc9
Petro Kirushkin <pkyriushkin@nymbus.com>
2021-06-08 18:11:09 +0300

    NBTDB-832: added steps to several tests

--- src/test/java/com/nymbus/frontoffice/depositaccountsmanagement/C22578_EditNewRegularSavingsAccountTest.java
+++ src/test/java/com/nymbus/frontoffice/depositaccountsmanagement/C22578_EditNewRegularSavingsAccountTest.java
@@ -79,34 +79,37 @@ public class C22578_EditNewRegularSavingsAccountTest extends BaseTest {
         AccountActions.editAccount().verifySavingsAccountFieldsAreDisabledForEditing();
 
         logInfo("Step 6: Select data in such drop-down fields that were not available in Add New mode:");
-        logInfo("Step 7: Fill in such text fields that were not displayed in Add new mode:");
-        logInfo("Step 8: Select any other value in such fields");
-        logInfo("Step 9: Set such switchers");
+        logInfo("Step 7: Look at the field 'Account Type' and verify that such field is not disabled for editing");
+        logInfo("Step 8: Click on the 'Account Type' drop-down and verify the account types that are present in the list");
+        logInfo("Step 9: Change account type to some value from the drop-down list (e.g. Officer)");
+        logInfo("Step 10: Fill in such text fields that were not displayed in Add new mode:");
+        logInfo("Step 11: Select any other value in such fields");
+        logInfo("Step 12: Set such switchers");
         AccountActions.editAccount().selectValuesInFieldsThatWereNotAvailableDuringSavingsAccountCreation(regularSavingsAccount);
 
-        logInfo("Step 10: Click [-] icon next to any section (e.g. Transactions section) and verify that all fields within this section were hidden");
+        logInfo("Step 13: Click [-] icon next to any section (e.g. Transactions section) and verify that all fields within this section were hidden");
         Pages.editAccountPage().clickMiscSectionLink();
 
-        logInfo("Step 11: Click [+] icon next to the section from Step9 and verify that all fields within the section are displayed. Fields were NOT cleared out");
+        logInfo("Step 14: Click [+] icon next to the section from Step9 and verify that all fields within the section are displayed. Fields were NOT cleared out");
         Pages.editAccountPage().clickMiscSectionLink();
 
-        logInfo("Step 12: Submit the account editing by clicking [Save] button");
+        logInfo("Step 15: Submit the account editing by clicking [Save] button");
         Pages.addAccountPage().clickSaveAccountButton();
         Pages.accountDetailsPage().waitForFullProfileButton();
 
-        logInfo("Step 13: Pay attention to Savings account fields");
+        logInfo("Step 16: Pay attention to Savings account fields");
         AccountActions.accountDetailsActions().clickMoreButton();
         AccountActions.verifyingAccountDataActions().verifySavingsAccountFieldsWithUpdatedDataInViewMode(regularSavingsAccount);
 
-        logInfo("Step 14: Click [Edit] button and pay attention to the fields");
+        logInfo("Step 17: Click [Edit] button and pay attention to the fields");
         Pages.accountDetailsPage().clickEditButton();
         AccountActions.editAccount().verifySavingsAccountFieldsWithUpdatedDataInEditMode(regularSavingsAccount);
 
-        logInfo("Step 15: Do not make any changes and go to Account Maintenance -> Maintenance History page");
+        logInfo("Step 18: Do not make any changes and go to Account Maintenance -> Maintenance History page");
         Pages.accountNavigationPage().clickMaintenanceTab();
         Pages.accountMaintenancePage().clickViewAllMaintenanceHistoryLink();
 
-        logInfo("Step 16: Look through the records on Maintenance History page and check that all fields that were filled in during account creation are reported in account Maintenance History");
+        logInfo("Step 19: Look through the records on Maintenance History page and check that all fields that were filled in during account creation are reported in account Maintenance History");
         AccountActions.accountMaintenanceActions().verifySavingsAccountRecordsAfterEditing(regularSavingsAccount);
     }
 }
