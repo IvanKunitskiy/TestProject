8d28fd631c6d5bd61cd43074b3dafecd31698e98
Petro Kirushkin <pkyriushkin@nymbus.com>
2021-06-16 15:22:03 +0300

    NBTDB-832: added initial C32541 template

---
+++ src/test/java/com/nymbus/frontoffice/loansmanagement/C32541_PaymentProcessing420ForceToPrinActivePDrecordAmountPrinPortion.java
@@ -0,0 +1,149 @@
+package com.nymbus.frontoffice.loansmanagement;
+
+import com.codeborne.selenide.Selenide;
+import com.nymbus.actions.Actions;
+import com.nymbus.actions.account.AccountActions;
+import com.nymbus.actions.client.ClientsActions;
+import com.nymbus.core.base.BaseTest;
+import com.nymbus.core.utils.DateTime;
+import com.nymbus.newmodels.account.Account;
+import com.nymbus.newmodels.account.loanaccount.PaymentAmountType;
+import com.nymbus.newmodels.account.product.AccountType;
+import com.nymbus.newmodels.account.product.Products;
+import com.nymbus.newmodels.account.product.RateType;
+import com.nymbus.newmodels.client.IndividualClient;
+import com.nymbus.newmodels.generation.client.builder.IndividualClientBuilder;
+import com.nymbus.newmodels.generation.client.builder.type.individual.IndividualBuilder;
+import com.nymbus.newmodels.generation.tansactions.TransactionConstructor;
+import com.nymbus.newmodels.generation.tansactions.builder.GLDebitDepositCHKAccBuilder;
+import com.nymbus.newmodels.transaction.Transaction;
+import com.nymbus.pages.Pages;
+import com.nymbus.testrail.TestRailIssue;
+import io.qameta.allure.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Frontoffice")
+@Feature("Loans Management")
+@Owner("Petro")
+public class C32541_PaymentProcessing420ForceToPrinActivePDrecordAmountPrinPortion extends BaseTest {
+
+    private Account loanAccount;
+    private Account chkAccount;
+    private Transaction transaction_420;
+    private String clientRootId;
+    private final String loanProductName = "Test Loan Product";
+    private final String loanProductInitials = "TLP";
+    private final String TEST_RUN_NAME = "Loans Management";
+
+    @BeforeMethod
+    public void preConditions(){
+
+        // Set up Client
+        IndividualClientBuilder individualClientBuilder = new IndividualClientBuilder();
+        individualClientBuilder.setIndividualClientBuilder(new IndividualBuilder());
+        IndividualClient client = individualClientBuilder.buildClient();
+
+        // Set up accounts
+        chkAccount = new Account().setCHKAccountData();
+        loanAccount = new Account().setLoanAccountData();
+        loanAccount.setProduct(loanProductName);
+        loanAccount.setMailCode(client.getIndividualClientDetails().getMailCode().getMailCode());
+        loanAccount.setPaymentAmountType(PaymentAmountType.PRINCIPAL_AND_INTEREST.getPaymentAmountType());
+
+        // Set proper dates
+        String localDate = DateTime.getLocalDateOfPattern("MM/dd/yyyy");
+        String dateMinusMonth = DateTime.getDateMinusMonth(localDate, 1);
+        loanAccount.setDateOpened(DateTime.getDateWithFormatPlusDays(dateMinusMonth, "MM/dd/yyyy", "MM/dd/yyyy", 2));
+        loanAccount.setNextPaymentBilledDueDate(DateTime.getDatePlusMonth(loanAccount.getDateOpened(), 1));
+        loanAccount.setPaymentBilledLeadDays("1");
+        loanAccount.setCycleLoan(false);
+        chkAccount.setDateOpened(DateTime.getDateMinusMonth(loanAccount.getDateOpened(), 1));
+
+        // Login to the system
+        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+
+        // Check that a Loan product exist with the following editable fields (Readonly? = NO) and create if not exist
+        Actions.loanProductOverviewActions().checkLoanProductExistAndCreateIfFalse(loanProductName, loanProductInitials);
+        Actions.loginActions().doLogOut();
+        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+
+        // Set products
+        chkAccount.setProduct(Actions.productsActions().getProduct(Products.CHK_PRODUCTS, AccountType.CHK, RateType.FIXED));
+
+        // Create client
+        ClientsActions.individualClientActions().createClient(client);
+        ClientsActions.individualClientActions().setClientDetailsData(client);
+        ClientsActions.individualClientActions().setDocumentation(client);
+
+        // Create account
+        AccountActions.createAccount().createCHKAccount(chkAccount);
+        Pages.accountNavigationPage().clickAccountsInBreadCrumbs();
+        AccountActions.createAccount().createLoanAccount(loanAccount);
+        clientRootId = ClientsActions.createClient().getClientIdFromUrl();
+
+        // Set up deposit transaction
+        int depositTransactionAmount = 12000;
+        Transaction depositTransaction = new TransactionConstructor(new GLDebitDepositCHKAccBuilder()).constructTransaction();
+        depositTransaction.getTransactionDestination().setAccountNumber(chkAccount.getAccountNumber());
+        depositTransaction.getTransactionDestination().setAmount(depositTransactionAmount);
+        depositTransaction.getTransactionSource().setAmount(depositTransactionAmount);
+
+        // Perform deposit transaction
+        Actions.loginActions().doLogOutProgrammatically();
+        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+        Actions.transactionActions().goToTellerPage();
+        Actions.transactionActions().doLoginTeller();
+        Actions.transactionActions().createTransaction(depositTransaction);
+        Actions.transactionActions().clickCommitButton();
+        Pages.tellerPage().closeModal();
+        Actions.nonTellerTransaction().generatePaymentDueRecord(clientRootId);
+
+        Actions.loginActions().doLogOutProgrammatically();
+
+    }
+
+    @TestRailIssue(issueID = 32541, testRunName = TEST_RUN_NAME)
+    @Test(description = "C32541, Payment Processing: 420 - Force To Prin, Active PD record, amount > prin portion")
+    @Severity(SeverityLevel.CRITICAL)
+    public void paymentProcessing420ForceToPrinActivePDrecordAmountPrinPortion() {
+
+        logInfo("Step 1: Log in to the NYMBUS");
+        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+
+        logInfo("Step 2: Go to the 'Teller' screen");
+        Pages.aSideMenuPage().clickClientMenuItem();
+        Actions.clientPageActions().searchAndOpenAccountByAccountNumber(loanAccount);
+        double currentBalanceBefore = Double.parseDouble(Pages.accountDetailsPage().getCurrentBalance());
+
+        Actions.transactionActions().goToTellerPage();
+
+        logInfo("Step 3: Log in to the proof date");
+        Actions.transactionActions().doLoginTeller();
+
+        logInfo("Step 4: Commit '420 - Force To Prin' transaction with the following fields:" +
+                "Sources -> Misc Debit:\n" +
+                "\n" +
+                "\"Account Number\" - active CHK or SAV account from preconditions\n" +
+                "\"Transaction Code\" - \"114 - Loan Payment\"\n" +
+                "Amount > Payment Info -> Payments Due record from preconditions -> Principal portion of the PD record with Loan from preconditions\n" +
+                "Destinations -> Misc Credit:\n" +
+                "\n" +
+                "Account number - Loan account from preconditions\n" +
+                "\"Transaction Code\" - \"420 - Force To Prin\"\n" +
+                "\"Amount\" - specify the same amount");
+
+        logInfo("Step 5: Close Transaction Receipt popup");
+        logInfo("Step 6: Open loan account from preconditions on the \"Transactions\" tab");
+        Pages.aSideMenuPage().clickClientMenuItem();
+        Actions.clientPageActions().searchAndOpenAccountByAccountNumber(loanAccount);
+        Selenide.sleep(10000000);
+
+        logInfo("Step 7: Go to the 'Payment Info' tab");
+        logInfo("Step 8: Verify existing Payment Due record");
+        logInfo("Step 9: Click on the Payment Due record and check fields in the 'Payment Due Details' section");
+        logInfo("Step 10: Pay attention to the 'Transactions' section");
+
+    }
+
+}
