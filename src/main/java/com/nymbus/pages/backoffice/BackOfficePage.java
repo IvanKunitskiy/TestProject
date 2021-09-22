88ef1c810a5b2d5d8f42b94f7d6e2839df0b5655
dbarsukov <dbarsukov@nymbus.com>
2021-09-22 19:43:44 +0300

    NBTDB-832: end test C22846

--- src/main/java/com/nymbus/pages/backoffice/BackOfficePage.java
+++ src/main/java/com/nymbus/pages/backoffice/BackOfficePage.java
@@ -7,29 +7,41 @@ import org.openqa.selenium.By;
 
 public class BackOfficePage extends PageTools {
 
-    private By reportGeneratorPageHeader = By.xpath("//h1[text()='Ad Hoc Reporting']");
-    private By documentSearchSeeMoreLink = By.xpath("//li/div/div[@class='header']/h2[text()='Document Search']/parent::div/following-sibling::div//div/div/a[text()='See more ']");
-    private By officialChecksButton = By.xpath("//h2[text()='Official Checks']/../..//a");
-    private By fileImportDropdown = By.xpath("(//span[contains(string(),'Select')])[2]");
-    private By fileImportSpan = By.xpath("//span[contains(string(),'%s')]");
-    private By importSpan = By.xpath("//span[contains(string(),'Import')]");
-    private By importButton = By.xpath("(//span[contains(string(),'Import')])[2]");
-    private By fileInput = By.xpath("//input[@type='file']");
-    private By cellImportTable = By.xpath("(//td)[%s]");
-    private By postButton = By.xpath("//span[contains(text(),\"Post\")]");
-    private By cellSpan = By.xpath("(//td)[%s]/span");
-    private By itemsToWorkButton = By.xpath("//h2[contains(text(), 'Items to Work')]//..//..//a[contains(text(), 'See more')]");
-    private By accountNumber = By.xpath("//div[@class='reject-item-summary-column ng-binding' and contains(string(),'%s')]");
-    private By date = By.xpath("//input[@data-test-id='field-']");
-    private By reason = By.xpath("(//span[contains(string(),'Reason')])[1]");
-    private By badCode = By.xpath("(//span[contains(string(),'bad code')])[1]");
-    private By filterButton = By.xpath("//button[contains(string(),'Filter')]");
-    private By editButton = By.xpath("//button[contains(string(),'Edit')]");
-    private By transactionCodeInput = By.xpath("(//a[@aria-label='Select box select'])[2]");
-    private By itemInTranCodeList = By.xpath("//ul[@repeat='item in tranCodeList']");
-    private By saveButton = By.xpath("//button[contains(string(),'Save')]");
-    private By createSwapButton = By.xpath("//button[contains(string(),'Create')]");
-    private By closeButton = By.xpath("//button[contains(string(),'Close')]");
+    private final By reportGeneratorPageHeader = By.xpath("//h1[text()='Ad Hoc Reporting']");
+    private final By documentSearchSeeMoreLink = By.xpath("//li/div/div[@class='header']/h2[text()='Document Search']/parent::div/following-sibling::div//div/div/a[text()='See more ']");
+    private final By officialChecksButton = By.xpath("//h2[text()='Official Checks']/../..//a");
+    private final By fileImportDropdown = By.xpath("(//span[contains(string(),'Select')])[2]");
+    private final By fileImportSpan = By.xpath("//span[contains(string(),'%s')]");
+    private final By importSpan = By.xpath("//span[contains(string(),'Import')]");
+    private final By importButton = By.xpath("(//span[contains(string(),'Import')])[2]");
+    private final By fileInput = By.xpath("//input[@type='file']");
+    private final By cellImportTable = By.xpath("(//td)[%s]");
+    private final By postButton = By.xpath("//span[contains(text(),\"Post\")]");
+    private final By cellSpan = By.xpath("(//td)[%s]/span");
+    private final By itemsToWorkButton = By.xpath("//h2[contains(text(), 'Items to Work')]//..//..//a[contains(text(), 'See more')]");
+    private final By accountNumber = By.xpath("//div[@class='reject-item-summary-column ng-binding' and contains(string(),'%s')]");
+    private final By date = By.xpath("//input[@data-test-id='field-']");
+    private final By reason = By.xpath("(//span[contains(string(),'Reason')])[1]");
+    private final By badCode = By.xpath("(//span[contains(string(),'bad code')])[1]");
+    private final By filterButton = By.xpath("//button[contains(string(),'Filter')]");
+    private final By editButton = By.xpath("//button[contains(string(),'Edit')]");
+    private final By transactionCodeInput = By.xpath("(//a[@aria-label='Select box select'])[2]");
+    private final By itemInTranCodeList = By.xpath("//ul[@repeat='item in tranCodeList']");
+    private final By saveButton = By.xpath("//button[contains(string(),'Save')]");
+    private final By createSwapButton = By.xpath("//button[contains(string(),'Create')]");
+    private final By closeButton = By.xpath("//button[contains(string(),'Close')]");
+    private final By statusDropDown = By.xpath("//a[@placeholder='Status']");
+    private final By status = By.xpath("(//span[contains(string(),'%s')])[1]");
+    private final By sourceDropDown = By.xpath("//a[@placeholder='Source']");
+    private final By proofDateByAccountNumber = By.xpath("(//div[@class='reject-item-summary-column ng-binding' and contains(string(),'%s')]/../*)[1]");
+    private final By sourceByAccountNumber = By.xpath("(//div[@class='reject-item-summary-column ng-binding' and contains(string(),'%s')]/../*)[3]");
+    private final By amountByAccountNumber = By.xpath("(//div[@class='reject-item-summary-column ng-binding' and contains(string(),'%s')]/../*)[9]");
+    private final By statusByAccountNumber = By.xpath("(//div[@class='reject-item-summary-column ng-binding' and contains(string(),'%s')]/../*)[10]");
+    private final By branchByAccountNumber = By.xpath("(//div[@class='reject-item-summary-column ng-binding' and contains(string(),'%s')]/../*)[7]");
+    private final By xSource= By.xpath("(//abbr[@ng-click='customClear($event)'])[1]");
+    private final By branchDropDown = By.xpath("//a[@placeholder='Branch']");
+    private final By branchOption = By.xpath("//li[@role='option']/div/span[contains(string(),'%s')]");
+    private final By showMore = By.xpath("//button[@ng-click='loadMoreHandler()']");
 
     @Step("Click the 'Official checks' button")
     public void clickOfficialChecks() {
@@ -166,4 +178,80 @@ public class BackOfficePage extends PageTools {
         waitForElementVisibility(closeButton);
         click(closeButton);
     }
+
+    @Step("Click 'Status' dropdown")
+    public void clickStatusDropDown() {
+        waitForElementVisibility(statusDropDown);
+        click(statusDropDown);
+    }
+
+    @Step("Click 'Status' or 'Source'")
+    public void clickStatusOrSource(String status) {
+        waitForElementVisibility(this.status, status);
+        click(this.status, status);
+    }
+
+    @Step("Click 'Source' dropdown")
+    public void clickSourceDropDown() {
+        waitForElementVisibility(sourceDropDown);
+        click(sourceDropDown);
+    }
+
+    @Step("Get 'Proof Date' by account number")
+    public String getProofDateByAccountNumber(String accountNumber) {
+        waitForElementVisibility(proofDateByAccountNumber, accountNumber);
+        return getElementText(proofDateByAccountNumber, accountNumber);
+    }
+
+    @Step("Get 'Source' by account number")
+    public String getSourceByAccountNumber(String accountNumber) {
+        waitForElementVisibility(sourceByAccountNumber, accountNumber);
+        return getElementText(sourceByAccountNumber, accountNumber);
+    }
+
+    @Step("Get 'Amount' by account number")
+    public String getAmountByAccountNumber(String accountNumber) {
+        waitForElementVisibility(amountByAccountNumber, accountNumber);
+        return getElementText(amountByAccountNumber, accountNumber);
+    }
+
+    @Step("Get 'Status' by account number")
+    public String getStatusByAccountNumber(String accountNumber) {
+        waitForElementVisibility(statusByAccountNumber, accountNumber);
+        return getElementText(statusByAccountNumber, accountNumber);
+    }
+
+    @Step("Get 'Branch' by account number")
+    public String getBranchByAccountNumber(String accountNumber) {
+        waitForElementVisibility(branchByAccountNumber, accountNumber);
+        return getElementText(branchByAccountNumber, accountNumber);
+    }
+
+    @Step("Click 'XSource' button")
+    public void clickXSource() {
+        waitForElementVisibility(xSource);
+        click(xSource);
+    }
+
+    @Step("Click 'Branch' dropdown")
+    public void clickBranchDropDown() {
+        waitForElementVisibility(branchDropDown);
+        click(branchDropDown);
+    }
+
+    @Step("Click 'Branch' option")
+    public void clickBranchOption(String branch) {
+        waitForElementVisibility(branchOption, branch);
+        click(branchOption, branch);
+    }
+
+    @Step("Click 'Show More' button")
+    public void clickShowMore() {
+        waitForElementVisibility(showMore);
+        waitForElementClickable(showMore);
+        click(showMore);
+    }
+
+
+
 }
