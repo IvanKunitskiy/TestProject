67e9e779f05b5ea26bc913173ce875bfa3deb2c7
Petro Kirushkin <pkyriushkin@nymbus.com>
2021-10-13 12:15:57 +0300

    NBTDB-832: created base test

--- src/test/java/com/nymbus/frontoffice/depositaccountsmanagement/C19439_CdCrCashInWithCashRecycler.java
+++ src/test/java/com/nymbus/frontoffice/depositaccountsmanagement/C19439_CdCrCashInWithCashRecycler.java
@@ -3,6 +3,7 @@ package com.nymbus.frontoffice.depositaccountsmanagement;
 import com.nymbus.actions.Actions;
 import com.nymbus.actions.account.AccountActions;
 import com.nymbus.actions.client.ClientsActions;
+import com.nymbus.actions.webadmin.WebAdminActions;
 import com.nymbus.core.base.BaseTest;
 import com.nymbus.core.utils.Constants;
 import com.nymbus.core.utils.DateTime;
@@ -26,18 +27,17 @@ import com.nymbus.newmodels.transaction.verifyingModels.BalanceData;
 import com.nymbus.newmodels.transaction.verifyingModels.CashDrawerData;
 import com.nymbus.newmodels.transaction.verifyingModels.TransactionData;
 import com.nymbus.pages.Pages;
-import com.nymbus.testrail.CustomStepResult;
-import com.nymbus.testrail.TestRailAssert;
 import com.nymbus.testrail.TestRailIssue;
 import io.qameta.allure.*;
+import org.testng.SkipException;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 @Epic("Frontoffice")
 @Feature("Transactions")
 @Owner("Petro")
-public class C19444_CdEcCashOutWithCashDrawer extends BaseTest {
-
+public class C19439_CdCrCashInWithCashRecycler extends BaseTest {
+    private String cashRecycler;
     IndividualClient client;
     private Account chkAccount;
     private TransactionData transactionData;
@@ -47,15 +47,19 @@ public class C19444_CdEcCashOutWithCashDrawer extends BaseTest {
     private final TransactionDestination cashOutDestination = DestinationFactory.getCashOutDestination();
 
     @BeforeMethod
-    public void preConditions(){
+    public void preConditions() {
+
         //Check CFMIntegrationEnabled
-//        WebAdminActions.loginActions().openWebAdminPageInNewWindow();
-//        WebAdminActions.loginActions().doLogin(userCredentials.getUserName(),userCredentials.getPassword());
-//        if (WebAdminActions.webAdminUsersActions().isCFMIntegrationEnabled()) {
-//            throw new SkipException("CFMIntegrationEnabled = 1");
-//        }
-//        WebAdminActions.loginActions().doLogout();
-//        WebAdminActions.loginActions().closeWebAdminPageAndSwitchToPreviousTab();
+        WebAdminActions.loginActions().openWebAdminPageInNewWindow();
+        WebAdminActions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+        if (WebAdminActions.webAdminUsersActions().isCFMIntegrationEnabled()) {
+            throw new SkipException("CFMIntegrationEnabled = 1");
+        }
+
+        cashRecycler = WebAdminActions.webAdminUsersActions().getCashRecyclerName();
+
+        WebAdminActions.loginActions().doLogout();
+        WebAdminActions.loginActions().closeWebAdminPageAndSwitchToPreviousTab();
 
         // Set up Client
         IndividualClientBuilder individualClientBuilder = new IndividualClientBuilder();
@@ -139,58 +143,11 @@ public class C19444_CdEcCashOutWithCashDrawer extends BaseTest {
     @TestRailIssue(issueID = 19444, testRunName = TEST_RUN_NAME)
     @Test(description = "C19444, [CD] EC Cash Out with Cash Drawer")
     @Severity(SeverityLevel.CRITICAL)
-    public void cdEcCashOutWithCashDrawer() {
-
+    public void cdCrCashInWithCashRecycler() {
         logInfo("Step 1: Log in to the system as the user from preconditions");
-        Actions.loginActions().doLogin(userCredentials.getUserName(), userCredentials.getPassword());
+        Actions.loginActions().doLogin(Constants.USERNAME, Constants.PASSWORD);
 
-        logInfo("Step 2: Go to Journal screen and log in to proof date with teller + cash drawer mentioned above");
-        Actions.journalActions().goToJournalPage();
-        Actions.transactionActions().doLoginTeller();
 
-        logInfo("Step 3: Search for transaction from preconditions and open its details");
-        Actions.journalActions().applyFilterByAccountNumber(chkAccount.getAccountNumber());
-        Actions.journalActions().clickLastTransaction();
-
-        logInfo("Step 4: Click [Error Correct] button");
-        Pages.journalDetailsPage().clickErrorCorrectButton();
-        Pages.journalDetailsPage().waitForErrorCorrectButtonInvisibility();
-        TestRailAssert.assertEquals(Actions.journalActions().getTransactionState(), "Void",
-                new CustomStepResult("Transaction state has changed", "Transaction state hasn't changed"));
-
-        logInfo("Step 5: Go to Cash Drawer screen, search for cash drawer used in teller session and verify its:\n" +
-                "- denominations\n" +
-                "- total cash out");
-        Pages.aSideMenuPage().clickCashDrawerMenuItem();
-        SelenideTools.sleep(Constants.MICRO_TIMEOUT);
-
-        CashDrawerData actualCashDrawerData = Actions.cashDrawerAction().getCashDrawerData();
-
-        TestRailAssert.assertEquals(actualCashDrawerData.getFiftiesAmount() + 100.00, cashDrawerData.getFiftiesAmount(),
-                new CustomStepResult("'Fifties' denomination is valid", "'Fifties' denomination is not valid"));
-
-        logInfo("Step 6: Go to account used in withdrawal item and verify its:\n" +
-                "- current balance\n" +
-                "- available balance\n" +
-                "- Transactions history");
-        Pages.aSideMenuPage().clickClientMenuItem();
-        Actions.clientPageActions().searchAndOpenAccountByAccountNumber(chkAccount);
-
-        TestRailAssert.assertEquals(AccountActions.retrievingAccountData().getCurrentBalance(),
-                balanceData.getCurrentBalance(),
-                new CustomStepResult("CHK account current balance is correct", "CHK account current balance is not correct!"));
-        TestRailAssert.assertEquals(AccountActions.retrievingAccountData().getAvailableBalance(),
-                balanceData.getAvailableBalance(),
-                new CustomStepResult("CHK account available balance is correct", "CHK account available balance is not correct!"));
-
-        logInfo("Step 7: Open account on the Transactions tab and verify the Error corrected transaction");
-        AccountActions.retrievingAccountData().goToTransactionsTab();
-        int offset = AccountActions.retrievingAccountData().getOffset();
-        TransactionData actualTransactionData = AccountActions.retrievingAccountData().getTransactionDataWithOffset(offset, 1);
-        TestRailAssert.assertEquals(actualTransactionData, transactionData,
-                new CustomStepResult("Transaction data is matching", "Transaction data doesn't match!"));
-        TestRailAssert.assertEquals(Pages.accountTransactionPage().getCheckingAccountTransactionEcColumnValue(1), "EC",
-                new CustomStepResult("'EC' is in 'EC' column", "'EC' is not in 'EC' column"));
 
     }
 
